2e5d8e076594a68832364dcb320dff1afb189482
Hardik Desai <hardik.desai.kcl@gmail.com>
2019-01-03 14:34:20 +0000

    Added additional placement columns and added NotificationHistory foreignKey

--- src/Sfa.Tl.Matching.Database/Tables/NotificationHistory.sql
+++ src/Sfa.Tl.Matching.Database/Tables/NotificationHistory.sql
@@ -2,11 +2,14 @@
 (
 	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
 	[EmailTemplateId] UNIQUEIDENTIFIER NOT NULL, 
+	[EntityRefId] UNIQUEIDENTIFIER NOT NULL, 
 	[Sender] NVARCHAR(50) NOT NULL, 
 	[Recipients] NVARCHAR(50) NOT NULL, 
 	[Subject] NVARCHAR(50) NOT NULL, 
 	[Body] NVARCHAR(500) NOT NULL, 
 	[Status] INT NOT NULL, 
 	[CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
+	CONSTRAINT [FK_Notification_Employer] FOREIGN KEY ([EntityRefId]) REFERENCES [Employer]([Id]),
+	CONSTRAINT [FK_Notification_Provider] FOREIGN KEY ([EntityRefId]) REFERENCES [Provider]([Id]),
 	CONSTRAINT [FK_Notification_EmailTemplate] FOREIGN KEY ([EmailTemplateId]) REFERENCES [EmailTemplate]([Id])
 )
