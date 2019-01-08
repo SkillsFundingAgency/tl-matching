cb2fb9d710a3d489510bc02581277d03b6e25215
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-01-08 11:11:19 +0000

    updated columns and foreign keys in IndustryPlacement and NotificationHistory tables

--- src/Sfa.Tl.Matching.Database/Tables/IndustryPlacement.sql
+++ src/Sfa.Tl.Matching.Database/Tables/IndustryPlacement.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[IndustryPlacement]
 (
 	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
-	[EmployerId] UNIQUEIDENTIFIER NULL, 
 	[RoutePathId] UNIQUEIDENTIFIER NULL, 
 	[AddressId] UNIQUEIDENTIFIER NULL,
 	[PlacementAvailable] INT NULL DEFAULT 0, 
@@ -9,9 +8,9 @@
 	[ContactedOn] DATETIME2 NULL, 
 	[NextContactOn] DATETIME2 NULL, 
 	[Resolution] NVARCHAR(100) NULL, 
+	[SpecialRequirements] NVARCHAR(500) NULL, 
 	[CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
 	[ModifiedOn] DATETIME2 NULL, 
-	CONSTRAINT [FK_IndustryPlacement_Employer] FOREIGN KEY ([EmployerId]) REFERENCES [Employer]([Id]),
 	CONSTRAINT [FK_IndustryPlacement_RoutePath] FOREIGN KEY ([RoutePathId]) REFERENCES [RoutePath]([Id]),
 	CONSTRAINT [FK_IndustryPlacement_Address] FOREIGN KEY ([AddressId]) REFERENCES [Address]([Id]),
 )
--- src/Sfa.Tl.Matching.Database/Tables/NotificationHistory.sql
+++ src/Sfa.Tl.Matching.Database/Tables/NotificationHistory.sql
@@ -2,11 +2,15 @@
 (
 	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
 	[EmailTemplateId] UNIQUEIDENTIFIER NOT NULL, 
+	[ContactId] UNIQUEIDENTIFIER NOT NULL, 
 	[Sender] NVARCHAR(50) NOT NULL, 
-	[Recipients] NVARCHAR(50) NOT NULL, 
-	[Subject] NVARCHAR(50) NOT NULL, 
+	[Recipients] NVARCHAR(100) NOT NULL, 
+	[Subject] NVARCHAR(100) NOT NULL, 
 	[Body] NVARCHAR(500) NOT NULL, 
 	[Status] INT NOT NULL, 
+	[CreatedUserName] NVARCHAR(100) NOT NULL, 
+	[CreatedUserEmail] NVARCHAR(100) NOT NULL, 
 	[CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
+	CONSTRAINT [FK_Notification_Contact] FOREIGN KEY ([ContactId]) REFERENCES [Contact]([Id]),
 	CONSTRAINT [FK_Notification_EmailTemplate] FOREIGN KEY ([EmailTemplateId]) REFERENCES [EmailTemplate]([Id])
 )
