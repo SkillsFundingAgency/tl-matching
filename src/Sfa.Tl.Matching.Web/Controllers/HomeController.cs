6598231ab8a4681d8cc8d9c98a3e14e8799fbee6
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-01-24 14:55:36 +0000

    Add Login Flow using Account and Home Controllers

--- src/Sfa.Tl.Matching.Web/Controllers/HomeController.cs
+++ src/Sfa.Tl.Matching.Web/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Sfa.Tl.Matching.Web.Models;
 
@@ -7,77 +6,34 @@ namespace Sfa.Tl.Matching.Web.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly MatchingConfiguration _configuration;
-
-        //public HomeController(MatchingConfiguration configuration)
-        //{
-        //    _configuration = configuration;
-        //}
-
-        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
-        [AllowAnonymous]
-        public IActionResult Start()
-        {
-            //TODO: Begin the journey
-            return View("Index");
-        }
-
-        [AllowAnonymous]
-        public IActionResult ContactUs()
-        {
-            return View();
-        }
-
-        [AllowAnonymous]
-        public IActionResult Cookies()
-        {
-            return View();
-        }
-
-        [AllowAnonymous]
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [AllowAnonymous]
-        public IActionResult Login()
-        {
-            return RedirectToAction("Index");
-        }
-
-        public IActionResult Signout()
-        {
-            //TODO: Sign-out using proper idams methods
-           return RedirectToAction("Index");
-
-            //Should just do this:
-            /*
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            await HttpContext.SignOutAsync(WsFederationDefaults.AuthenticationScheme,
-            new AuthenticationProperties
-            {
-                RedirectUri = "/"
-            });
-             */
-        }
-
-        //public Task SignOutCleanup()
-        //{
-        //    //TODO: Confirm whether this method is needed
-        //}
-
-        [AllowAnonymous]
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        public IActionResult InvalidRole()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Cookies()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
