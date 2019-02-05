e5556a25c0a97fa864265d00411b47500012ba25
Adam <adamesfa@outlook.com>
2019-02-05 13:22:50 +0000

    Add validation codes

---
+++ src/Sfa.Tl.Matching.Models/Enums/ValidationErrorCode.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Sfa.Tl.Matching.Models.Enums
+{
+    public enum ValidationErrorCode
+    {
+        [Description("Wrong data type")]
+        WrongDataType = 1,
+        [Description("Wrong number of columns")]
+        WrongNumberOfColumns,
+        [Description("Missing mandatory data")]
+        MissingMandatoryData,
+        [Description("Invalid format")]
+        InvalidFormat
+    }
+}
