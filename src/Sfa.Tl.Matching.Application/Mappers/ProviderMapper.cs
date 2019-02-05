9431eb4dd2407e87a22f85b30b0ddc403c028d3a
Adam <adamesfa@outlook.com>
2019-02-05 16:06:22 +0000

    Create Provider mapper

---
+++ src/Sfa.Tl.Matching.Application/Mappers/ProviderMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper.Configuration;
+using Sfa.Tl.Matching.Models.Dto;
+
+namespace Sfa.Tl.Matching.Application.Mappers
+{
+    public class ProviderMapper : MapperConfigurationExpression
+    {
+        public ProviderMapper()
+        {
+            CreateMap<ProviderDto, Domain.Models.Provider>();
+        }
+    }
+}
