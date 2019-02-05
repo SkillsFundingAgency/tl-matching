38b5664a1ebfa4c7dc39bb84e7c7fb17bd48456e
Mike Wild <mike.wild@digital.education.gov.uk>
2019-02-05 17:09:35 +0000

    Add new RoutePathMapping to DB Context

---
+++ src/Sfa.Tl.Matching.Domain/Models/RoutePathMapping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// ReSharper disable UnusedMember.Global
+namespace Sfa.Tl.Matching.Domain.Models
+{
+    public class RoutePathMapping
+    {
+        public int Id { get; set; }
+        public string LarsId { get; set; }
+        public string Title { get; set; }
+        public string ShortTitle { get; set; }
+        public int PathId { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+
+        public virtual Path Path { get; set; }
+    }
+}
