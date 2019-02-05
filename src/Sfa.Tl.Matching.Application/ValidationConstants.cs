7a11745674a687a3088ca481b88cdab3e2ea4426
Adam <adamesfa@outlook.com>
2019-02-05 13:28:51 +0000

    Add validation constants

---
+++ src/Sfa.Tl.Matching.Application/ValidationConstants.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Sfa.Tl.Matching.Application
+{
+    public static class ValidationConstants
+    {
+        public static Regex UkprnRegex => 
+            new Regex(@"^((?!(0))[0-9]{8})$");
+
+        public static Regex PhoneNumberRegex => 
+            new Regex(@"^[0-9+\s-()]{8,16}$");
+    }
+}
