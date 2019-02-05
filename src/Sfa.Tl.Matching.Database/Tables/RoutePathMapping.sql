cbd4f6c8682cac3a6e35fa299ae4bc68fff0a2f1
Mike Wild <mike.wild@digital.education.gov.uk>
2019-02-05 12:21:44 +0000

    (TLWP-321) Add RoutePathMapping table

---
+++ src/Sfa.Tl.Matching.Database/Tables/RoutePathMapping.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[RoutePathMapping]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+	[LarsId] NVARCHAR(8) NOT NULL,
+	[Title] NVARCHAR(250) NOT NULL, 
+	[ShortTitle] NVARCHAR(50) NULL, 
+	[PathId] INT NOT NULL, 
+	[CreatedOn] DATETIME2 NULL DEFAULT GetDate(), 
+	[ModifiedOn] DATETIME2 NULL,
+
+	CONSTRAINT [FK_RoutePathMapping_Path] FOREIGN KEY ([PathId]) REFERENCES [Path]([Id])
+)
