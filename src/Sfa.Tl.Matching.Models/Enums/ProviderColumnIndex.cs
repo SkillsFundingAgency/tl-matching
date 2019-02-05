5abf9f64b26f26d6266521f25b070f31bf155a73
Adam <adamesfa@outlook.com>
2019-02-05 13:22:18 +0000

    Add Provider column indexes

---
+++ src/Sfa.Tl.Matching.Models/Enums/ProviderColumnIndex.cs
@@ -0,0 +1,18 @@
+﻿namespace Sfa.Tl.Matching.Models.Enums
+{
+    public enum ProviderColumnIndex
+    {
+        UkPrn = 0,
+        Name,
+        OfstedRating,
+        Active,
+        ActiveReason,
+        PrimaryContact,
+        PrimaryContactEmail,
+        PrimaryContactPhone,
+        SecondaryContact,
+        SecondaryContactEmail,
+        SecondaryContactPhone,
+        Source
+    }
+}
