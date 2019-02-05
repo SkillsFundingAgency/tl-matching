0c45f6c1c2364baecfc81e1641b641a8e40154fb
Adam <adamesfa@outlook.com>
2019-02-05 15:30:37 +0000

    Add description

--- src/Sfa.Tl.Matching.Models/Enums/OfstedRating.cs
+++ src/Sfa.Tl.Matching.Models/Enums/OfstedRating.cs
@@ -1,9 +1,12 @@
-﻿namespace Sfa.Tl.Matching.Models.Enums
+﻿using System.ComponentModel;
+
+namespace Sfa.Tl.Matching.Models.Enums
 {
     public enum OfstedRating
     {
         Outstanding = 1,
         Good,
+        [Description("Requires improvement")]
         RequiresImprovement,
         Inadequate
     }
