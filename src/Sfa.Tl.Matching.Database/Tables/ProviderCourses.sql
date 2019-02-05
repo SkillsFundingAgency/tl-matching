0202f96013c8197f9e8cd78aa7e672b3c9017e1b
Adam <adamesfa@outlook.com>
2019-02-05 13:21:55 +0000

    Remove FK

--- src/Sfa.Tl.Matching.Database/Tables/ProviderCourses.sql
+++ src/Sfa.Tl.Matching.Database/Tables/ProviderCourses.sql
@@ -4,7 +4,6 @@
 	[ProviderId] UNIQUEIDENTIFIER NOT NULL, 
 	[CourseId] UNIQUEIDENTIFIER NOT NULL, 
 	[AddressId] UNIQUEIDENTIFIER NOT NULL, 
-	CONSTRAINT [FK_ProviderCourse_Provider] FOREIGN KEY ([ProviderId]) REFERENCES [Provider]([Id]), 
 	CONSTRAINT [FK_ProviderCourse_Course] FOREIGN KEY ([CourseId]) REFERENCES [Course]([Id]),
 	CONSTRAINT [FK_ProviderCourse_Address] FOREIGN KEY ([AddressId]) REFERENCES [Address]([Id]), 
 )
