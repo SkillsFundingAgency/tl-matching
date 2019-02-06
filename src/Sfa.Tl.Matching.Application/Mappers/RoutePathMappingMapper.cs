a06b3cff61e208e674b3ab593d9dd4cda23f6a2e
Mike Wild <mike.wild@digital.education.gov.uk>
2019-02-06 16:07:45 +0000

    Remove commented lines from RoutePathMappingMapper

--- src/Sfa.Tl.Matching.Application/Mappers/RoutePathMappingMapper.cs
+++ src/Sfa.Tl.Matching.Application/Mappers/RoutePathMappingMapper.cs
@@ -8,10 +8,6 @@ namespace Sfa.Tl.Matching.Application.Mappers
         public RoutePathMappingMapper()
         {
             CreateMap<RoutePathMappingDto, Domain.Models.RoutePathMapping>();
-            //.ForMember(dest => dest.Id, mapping => mapping.Ignore())
-            //.ForMember(dest => dest.CreatedOn, mapping => mapping.Ignore())
-            //.ForMember(dest => dest.ModifiedOn, mapping => mapping.Ignore())
-            //.ForMember(dest => dest.Path, mapping => mapping.Ignore());
         }
     }
 }
