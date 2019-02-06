5acdae4760296d0c32b145ca390fb4c93dd1c3ea
Adam <adamesfa@outlook.com>
2019-02-06 15:31:56 +0000

    Add Employer and Provider DI

--- src/Sfa.Tl.Matching.Functions.Extensions/InjectConfiguration.cs
+++ src/Sfa.Tl.Matching.Functions.Extensions/InjectConfiguration.cs
@@ -2,11 +2,16 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using AutoMapper;
+using FluentValidation;
 using Microsoft.Azure.WebJobs.Host.Config;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Sfa.Tl.Matching.Application.FileReader;
+using Sfa.Tl.Matching.Application.FileReader.Employer;
+using Sfa.Tl.Matching.Application.FileReader.Provider;
 using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Application.Services;
 using Sfa.Tl.Matching.Data;
 using Sfa.Tl.Matching.Data.Interfaces;
 using Sfa.Tl.Matching.Data.Repositories;
@@ -41,27 +46,64 @@ namespace Sfa.Tl.Matching.Functions.Extensions
                     Environment.GetEnvironmentVariable("ServiceName"))
                 .Result;
 
+            services.AddLogging();
+
+            services.AddAutoMapper();
+
             services.AddDbContext<MatchingDbContext>(options =>
                 options.UseSqlServer(_configuration.SqlConnectionString));
 
-            services.AddAutoMapper();
-            //TODO: Remove SuppressMessage(s) on method
+            RegisterFileReaders(services);
 
-            services.AddTransient<IRepository<Employer>, EmployerRepository>();
-            services.AddTransient<IFileReader<EmployerDto>, ExcelFileReader<EmployerDto>>();
+            RegisterRepositories(services);
+
+            RegisterApplicationServices(services);
         }
 
-        private void RegisterFileReaders(IServiceCollection services)
+        private static void RegisterFileReaders(IServiceCollection services)
         {
-            // Just return if we've already added AutoMapper to avoid double-registration
-            //if (services.Any(sd => sd.ServiceType == typeof(IMapper)))
-            //    return services;
+            RegisterEmployerFileReader(services);
+            RegisterProviderFileReader(services);
+        }
 
-            var allTypes = AppDomain.CurrentDomain.GetAssemblies()
-                .Where(a => a.GetName().Name != nameof(AutoMapper))
-                .SelectMany(a => a.DefinedTypes)
-                .ToArray();
+        private static void RegisterEmployerFileReader(IServiceCollection services)
+        {
+            services.AddTransient<IDataParser<EmployerDto>, EmployerDataParser>();
+            services.AddTransient<IValidator<string[]>, EmployerDataValidator>();
 
+            services.AddTransient<IFileReader<EmployerDto>, ExcelFileReader<EmployerDto>>(employer =>
+                new ExcelFileReader<EmployerDto>(
+                    employer.GetService<ILogger<ExcelFileReader<EmployerDto>>>(),
+                    employer.GetService<IDataParser<EmployerDto>>(),
+                    (IValidator<string[]>)employer.GetServices(typeof(IValidator<string[]>)).Single(t => t.GetType() == typeof(EmployerDataValidator))));
+
+            services.AddTransient<IDataImportService<EmployerDto>, DataImportService<EmployerDto>>();
+        }
+
+        private static void RegisterProviderFileReader(IServiceCollection services)
+        {
+            services.AddTransient<IDataParser<ProviderDto>, ProviderDataParser>();
+            services.AddTransient<IValidator<string[]>, ProviderDataValidator>();
+
+            services.AddTransient<IFileReader<ProviderDto>, ExcelFileReader<ProviderDto>>(provider =>
+                new ExcelFileReader<ProviderDto>(
+                    provider.GetService<ILogger<ExcelFileReader<ProviderDto>>>(),
+                    provider.GetService<IDataParser<ProviderDto>>(),
+                    (IValidator<string[]>)provider.GetServices(typeof(IValidator<string[]>)).Single(t => t.GetType() == typeof(ProviderDataValidator))));
+
+            services.AddTransient<IDataImportService<ProviderDto>, DataImportService<ProviderDto>>();
+        }
+
+        private static void RegisterRepositories(IServiceCollection services)
+        {
+            services.AddTransient<IRepository<Employer>, EmployerRepository>();
+            services.AddTransient<IRepository<Provider>, ProviderRepository>();
+        }
+
+        private static void RegisterApplicationServices(IServiceCollection services)
+        {
+            services.AddTransient<IEmployerService, EmployerService>();
+            services.AddTransient<IProviderService, ProviderService>();
         }
     }
 }
