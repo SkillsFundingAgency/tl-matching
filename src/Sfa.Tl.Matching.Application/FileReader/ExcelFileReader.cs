8997e8d4d4701b39253e6de58bdc0f0222726aac
Adam <adamesfa@outlook.com>
2019-02-06 09:32:32 +0000

    Add logger to ExcelFileReader and ignore the first header row

--- src/Sfa.Tl.Matching.Application/FileReader/ExcelFileReader.cs
+++ src/Sfa.Tl.Matching.Application/FileReader/ExcelFileReader.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.Logging;
 using Sfa.Tl.Matching.Application.FileReader.Extensions;
 using Sfa.Tl.Matching.Application.Interfaces;
 
@@ -11,42 +13,52 @@ namespace Sfa.Tl.Matching.Application.FileReader
 {
     public class ExcelFileReader<TDto> : IFileReader<TDto> where TDto : class, new()
     {
+        private readonly ILogger<ExcelFileReader<TDto>> _logger;
         private readonly IDataParser<TDto> _dataParser;
         private readonly IValidator<string[]> _validator;
 
         public ExcelFileReader(
+            ILogger<ExcelFileReader<TDto>> logger,
             IDataParser<TDto> dataParser, 
             IValidator<string[]> validator)
         {
             _dataParser = dataParser;
             _validator = validator;
+            _logger = logger;
         }
 
         public IEnumerable<TDto> ValidateAndParseFile(Stream stream)
         {
-            var employers = new List<TDto>();
+            var dtos = new List<TDto>();
 
             using (var document = SpreadsheetDocument.Open(stream, false))
             {
                 var rows = OpenSpreadSheetAndReadAllGetRows(document);
-                
+
                 //NOTE:We May have some logic to do some kind or header validation
                 //rows.RemoveAt(0);
 
+                var rowCount = 0;
                 foreach (var row in rows)
                 {
-                    var cellValues = row.Descendants<Cell>().ToStringArray(document.WorkbookPart.SharedStringTablePart);
+                    rowCount++;
+                    if (rowCount == 1)
+                        continue;
 
-                    var validationResult = _validator.Validate(cellValues);
+                    var cellValues = row.Descendants<Cell>().ToStringArray(document.WorkbookPart.SharedStringTablePart).ToArray();
                     
-                    if(validationResult.IsValid)
+                    var validationResult = _validator.Validate(cellValues);
+                    if (!validationResult.IsValid)
                     {
-                        var fileEmployer = _dataParser.Parse(cellValues);
-                        employers.Add(fileEmployer);
-                    }
+                        var errorMessage = GetErrorMessage(rowCount, validationResult);
+                        _logger.LogError(errorMessage);
+                    };
+
+                    var dto = _dataParser.Parse(cellValues);
+                    dtos.Add(dto);
                 }
 
-                return employers;
+                return dtos;
             }
         }
 
@@ -61,5 +73,12 @@ namespace Sfa.Tl.Matching.Application.FileReader
             return sheetData.Descendants<Row>();
         }
 
+        private static string GetErrorMessage(int rowCount, ValidationResult validationResult)
+        {
+            var errorMessage =
+                $"Row Number={rowCount} failed with the following errors: \n{string.Join(", ", validationResult.Errors)}";
+
+            return errorMessage;
+        }
     }
 }
