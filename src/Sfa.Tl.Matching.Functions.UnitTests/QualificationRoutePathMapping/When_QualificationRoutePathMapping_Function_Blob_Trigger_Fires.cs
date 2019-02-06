e30b7b3c70f6f850819fe22f641c4d3307a4c8b5
Mike Wild <mike.wild@digital.education.gov.uk>
2019-02-06 08:04:56 +0000

    Add Functions unit tests project, with one initial test.

---
+++ src/Sfa.Tl.Matching.Functions.UnitTests/QualificationRoutePathMapping/When_QualificationRoutePathMapping_Function_Blob_Trigger_Fires.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using NSubstitute;
+using Sfa.Tl.Matching.Application.Interfaces;
+
+namespace Sfa.Tl.Matching.Functions.UnitTests.QualificationRoutePathMapping
+{
+    public class When_QualificationRoutePathMapping_Function_Blob_Trigger_Fires
+    {
+        private Stream _blobStream;
+        private ILogger _logger;
+        private IMapper _mapper;
+        private IRoutePathService _routePathService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            _blobStream = new MemoryStream();
+            _routePathService = Substitute.For<IRoutePathService>();
+            _logger = Substitute.For<ILogger>();
+            _mapper = Substitute.For<IMapper>();
+            Functions.QualificationRoutePathMapping.ImportQualificationRoutePathMapping(_blobStream, "test", _logger, _mapper, _routePathService);
+        }
+
+        [Test]
+        public void ImportQualificationPathMapping_Is_Called_Exactly_Once()
+        {
+            _routePathService
+                .Received(1)
+                .ImportQualificationPathMapping(
+                    Arg.Is(_blobStream));
+        }
+    }
+}
