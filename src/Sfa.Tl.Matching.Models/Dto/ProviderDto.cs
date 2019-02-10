be30c161b7fce41fbf1ad194938b3a63241a6ed2
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-02-10 13:02:57 +0000

    Provider rename Active to Status in Provider Table

--- src/Sfa.Tl.Matching.Models/Dto/ProviderDto.cs
+++ src/Sfa.Tl.Matching.Models/Dto/ProviderDto.cs
@@ -1,17 +1,15 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Sfa.Tl.Matching.Models.Enums;
+﻿using Sfa.Tl.Matching.Models.Enums;
 
 namespace Sfa.Tl.Matching.Models.Dto
 {
-    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public class ProviderDto
     {
         public int Id { get; set; }
         public long UkPrn { get; set; }
         public string Name { get; set; }
         public OfstedRating OfstedRating { get; set; }
-        public bool Active { get; set; }
-        public string ActiveReason { get; set; }
+        public bool Status { get; set; }
+        public string StatusReason { get; set; }
         public string PrimaryContact { get; set; }
         public string PrimaryContactEmail { get; set; }
         public string PrimaryContactPhone { get; set; }
