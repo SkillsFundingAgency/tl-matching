0d03841f1d7d39f056fd4459c9d1b7367ac2933f
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-02-11 09:14:26 +0000

    updated Employer and ProviderVenue FileImport Dto

--- src/Sfa.Tl.Matching.Models/Dto/EmployerFileImportDto.cs
+++ src/Sfa.Tl.Matching.Models/Dto/EmployerFileImportDto.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Sfa.Tl.Matching.Models.Dto
 {
@@ -9,14 +11,14 @@ namespace Sfa.Tl.Matching.Models.Dto
         public override int? NumberOfHeaderRows => 1;
 
         [Column(Order = 0)] public string CrmId { get; set; }
-        [Column(Order = 1)] public string CompanyName { get; set; }
-        [Column(Order = 2)] public string AlsoKnownAs { get; set; }
-        [Column(Order = 3)] public string Aupa { get; set; }
-        [Column(Order = 4)] public string CompanyType { get; set; }
-        [Column(Order = 4)] public string PrimaryContact { get; set; }
-        [Column(Order = 6)] public string Phone { get; set; }
-        [Column(Order = 7)] public string Email { get; set; }
-        [Column(Order = 8)] public string PostCode { get; set; }
-        [Column(Order = 9)] public string Owner { get; set; }
+        [Column(Order = 3)] public string CompanyName { get; set; }
+        [Column(Order = 4)] public string AlsoKnownAs { get; set; }
+        [Column(Order = 5)] public string Aupa { get; set; }
+        [Column(Order = 6)] public string CompanyType { get; set; }
+        [Column(Order = 7)] public string PrimaryContact { get; set; }
+        [Column(Order = 8)] public string Phone { get; set; }
+        [Column(Order = 9)] public string Email { get; set; }
+        [Column(Order = 10)] public string PostCode { get; set; }
+        [Column(Order = 11)] public string Owner { get; set; }
     }
 }
