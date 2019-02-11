7fa874baad6ed9348783c20729835734088bcb60
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-02-11 09:15:11 +0000

    Added Employer Integration Tests

---
+++ src/Sfa.Tl.Matching.Application.IntegrationTests/Employer/When_Provider_Imports_File_With_Missing_Mandatory.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Sfa.Tl.Matching.Models.Dto;
+
+namespace Sfa.Tl.Matching.Application.IntegrationTests.Employer
+{
+    public class When_Employer_Import_File_Has_Missing_Mandatory : EmployerTestBase
+    {
+        private const string DataFilePath = @"Employer\Employer-MissingMandatory.xlsx";
+        private int _createdRecordCount;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            await ResetData();
+
+            var filePath = Path.Combine(TestContext.CurrentContext.TestDirectory, DataFilePath);
+            using (var stream = File.Open(filePath, FileMode.Open))
+            {
+                _createdRecordCount = EmployerService.ImportEmployer(new EmployerFileImportDto { FileDataStream = stream }).Result;
+            }
+        }
+
+        [Test]
+        public void Then_No_Record_Is_Saved()
+        {
+            Assert.AreEqual(0, _createdRecordCount);
+        }
+    }
+}
