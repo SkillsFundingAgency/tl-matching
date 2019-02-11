eef659e2aef5a6a65bf17956c9e5660e5f1d73b2
Mike Wild <mike.wild@digital.education.gov.uk>
2019-02-11 11:27:27 +0000

    Remove unused usings from EmployerFileImportDto

--- src/Sfa.Tl.Matching.Models/Dto/EmployerFileImportDto.cs
+++ src/Sfa.Tl.Matching.Models/Dto/EmployerFileImportDto.cs
@@ -1,7 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
-using System.Text.RegularExpressions;
 
 namespace Sfa.Tl.Matching.Models.Dto
 {
