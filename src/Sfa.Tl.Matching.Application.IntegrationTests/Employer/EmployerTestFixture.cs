04f170fa8256b470e9127b851899920ac55e7553
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-02-11 13:22:01 +0000

    EmployerTestFixture and TestHelper

---
+++ src/Sfa.Tl.Matching.Application.IntegrationTests/Employer/EmployerTestFixture.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Sfa.Tl.Matching.Application.FileReader;
+using Sfa.Tl.Matching.Application.FileReader.Employer;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Application.Mappers;
+using Sfa.Tl.Matching.Application.Services;
+using Sfa.Tl.Matching.Data;
+using Sfa.Tl.Matching.Data.Repositories;
+using Sfa.Tl.Matching.Models.Dto;
+
+namespace Sfa.Tl.Matching.Application.IntegrationTests.Employer
+{
+
+    public class EmployerTestFixture : IDisposable
+    {
+        public IEmployerService EmployerService;
+        public MatchingDbContext MatchingDbContext;
+
+        public EmployerTestFixture()
+        {
+            var loggerRepository = new Logger<EmployerRepository>(
+                new NullLoggerFactory());
+
+            var loggerExcelFileReader = new Logger<ExcelFileReader<EmployerFileImportDto, EmployerDto>>(
+                new NullLoggerFactory());
+
+            MatchingDbContext = new TestConfiguration().GetDbContext();
+
+            var repository = new EmployerRepository(loggerRepository, MatchingDbContext);
+            var dataValidator = new EmployerDataValidator();
+            var dataParser = new EmployerDataParser();
+
+            var excelFileReader = new ExcelFileReader<EmployerFileImportDto, EmployerDto>(loggerExcelFileReader, dataParser, dataValidator);
+
+            var config = new MapperConfiguration(c => c.AddProfile<EmployerMapper>());
+
+            var mapper = new Mapper(config);
+
+            EmployerService = new EmployerService(mapper, excelFileReader, repository);
+        }
+
+        public void ResetData(string companyName)
+        {
+            var employer = MatchingDbContext.Employer.FirstOrDefault(e => e.CompanyName == companyName);
+
+            if (employer != null)
+            {
+                MatchingDbContext.Employer.Remove(employer);
+                var count = MatchingDbContext.SaveChanges();
+                count.Should().Be(1);
+            }
+        }
+
+        public void Dispose()
+        {
+            MatchingDbContext?.Dispose();
+        }
+    }
+}
---
+++ src/Sfa.Tl.Matching.Application.IntegrationTests/TestHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Sfa.Tl.Matching.Application.IntegrationTests
+{
+    public static class TestHelper
+    {
+        public static string GetTestExecutionDirectory()
+        {
+            var codeBaseUrl = new Uri(Assembly.GetExecutingAssembly().CodeBase);
+            var codeBasePath = Uri.UnescapeDataString(codeBaseUrl.AbsolutePath);
+            return Path.GetDirectoryName(codeBasePath);
+        }
+    }
+}
