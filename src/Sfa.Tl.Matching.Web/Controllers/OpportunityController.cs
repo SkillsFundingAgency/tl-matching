0c3e3d22f4823e519c4f63818b270d5e047c5ffa
Adam <adamesfa@outlook.com>
2019-02-18 15:15:55 +0000

    Add OpportunityController

--- src/Sfa.Tl.Matching.Web/Controllers/OpportunityController.cs
+++ src/Sfa.Tl.Matching.Web/Controllers/OpportunityController.cs
@@ -1,19 +1,33 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.Matching.Application.Interfaces;
 using Sfa.Tl.Matching.Infrastructure.Extensions;
+using Sfa.Tl.Matching.Models.ViewModel;
 
 namespace Sfa.Tl.Matching.Web.Controllers
 {
     [Authorize(Roles = RolesExtensions.StandardUser + "," + RolesExtensions.AdminUser)]
     public class OpportunityController : Controller
     {
-        public OpportunityController()
+        private readonly IOpportunityService _opportunityService;
+
+        public OpportunityController(IOpportunityService opportunityService)
         {
+            _opportunityService = opportunityService;
+        }
+        
+        [HttpGet("placement-information", Name = RouteNames.PlacementsGet)]
+        public IActionResult Placements()
+        {
+            var viewModel = new PlacementInformationViewModel();
+
+            return View(viewModel);
         }
 
-        public IActionResult Index()
+        [HttpPost("placement-information", Name = RouteNames.PlacementsPost)]
+        public IActionResult Placements(PlacementInformationViewModel viewModel)
         {
-            return View();
+            return RedirectToRoute(RouteNames.EmployerNameGet);
         }
     }
 }
