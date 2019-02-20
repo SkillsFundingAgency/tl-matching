f8cda9930c1aef8346e91d46e75221d9951216fd
Adam <adamesfa@outlook.com>
2019-02-20 09:03:33 +0000

    Add Route Names and Templates

---
+++ src/Sfa.Tl.Matching.Web/Constants/RouteNames.cs
@@ -0,0 +1,12 @@
+﻿namespace Sfa.Tl.Matching.Web.Constants
+{
+    public static class RouteNames
+    {
+        public const string EmployerNameGet = "EmployerName_Get";
+        public const string EmployerNamePost = "EmployerName_Post";
+        public const string EmployerDetailsGet = "EmployerDetails_Get";
+        public const string EmployerDetailsPost = "EmployerDetails_Post";
+        public const string PlacementsGet = "Placements_Get";
+        public const string PlacementsPost = "Placements_Post";
+    }
+}
---
+++ src/Sfa.Tl.Matching.Web/Constants/RouteTemplates.cs
@@ -0,0 +1,9 @@
+﻿namespace Sfa.Tl.Matching.Web.Constants
+{
+    public static class RouteTemplates
+    {
+        public const string EmployerName = "who-is-employer";
+        public const string EmployerDetails = "employer-details";
+        public const string PlacementInformation = "placement-information";
+    }
+}
