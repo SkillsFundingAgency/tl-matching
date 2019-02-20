a9626f160b12c1bee87928b80a295ca16d78cc7a
Adam <adamesfa@outlook.com>
2019-02-20 09:03:52 +0000

    Add Min and Max validation attributes

---
+++ src/Sfa.Tl.Matching.Models/ValidationAttributes/MaxAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sfa.Tl.Matching.Models.ValidationAttributes
+{
+    public class MaxAttribute : ValidationAttribute
+    {
+        private readonly short _maxValue;
+
+        public MaxAttribute(short maxValue)
+        {
+            _maxValue = maxValue;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return (short)value <= _maxValue;
+        }
+    }
+}
---
+++ src/Sfa.Tl.Matching.Models/ValidationAttributes/MinAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sfa.Tl.Matching.Models.ValidationAttributes
+{
+    public class MinAttribute : ValidationAttribute
+    {
+        private readonly short _minValue;
+
+        public MinAttribute(short minValue)
+        {
+            _minValue = minValue;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return (short)value >= _minValue;
+        }
+    }
+}
