8adbb71ee875ab0d7d840f8810a440c4f3c11140
Ashley Anderton <ashley.j.anderton@gmail.com>
2019-02-22 11:33:39 +0000

    Reworked gulp to merge all js into single file, same with CSS. Refs in scripts and head

---
+++ src/Sfa.Tl.Matching.Web/Frontend/gulpfile.js
@@ -0,0 +1,31 @@
+﻿var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var concatCss = require('gulp-concat-css');
+
+gulp.task('default', ['copy-js', 'copy-css', 'copy-assets']);
+
+gulp.task('copy-js', function () {
+    return gulp.src([
+        'node_modules/accessible-autocomplete/dist/accessible-autocomplete.min.js',
+        'src/javascripts/*.js'
+    ])
+        .pipe(concat('javascripts/all.js'))
+        .pipe(gulp.dest('../wwwroot/'))
+});
+
+gulp.task('copy-css', function () {
+    return gulp.src([
+        'src/stylesheets/*.css', 
+        'node_modules/accessible-autocomplete/dist/accessible-autocomplete.min.css'
+    ])
+        .pipe(concatCss('stylesheets/all.css'))
+        .pipe(gulp.dest('../wwwroot/'))
+});
+
+
+gulp.task('copy-assets', () => {
+    gulp.src('src/assets/**/*')
+        .pipe(gulp.dest('../wwwroot/assets'))
+});
+
