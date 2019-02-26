9d75b7788ced06ff647516e19dad2ebe3c7c89fe
Ashley Anderton <ashley.j.anderton@gmail.com>
2019-02-26 12:05:04 +0000

    Removed reliance on DAS frontend, implemented gulp compile of all SASS. Removed DAS references from front end code, replaced with TL classes.

---
+++ src/Sfa.Tl.Matching.Web/Frontend/README.md
@@ -0,0 +1,21 @@
+# tl-matching
+
+
+## Frontend
+
+
+## Requirements 
+
+1. node - https://nodejs.org/en/
+2. npm - https://www.npmjs.com/package/npm
+3. Gulp - https://gulpjs.com/
+
+## Actions
+cd to "Frontend" directory
+
+|Task|Description|
+|----|-----------|
+| `npm install` | Installs all node packages |
+| `gulp` | Merges, compiles and moves all sass / js / assets |
+
+
--- src/Sfa.Tl.Matching.Web/Frontend/gulpfile.js
+++ src/Sfa.Tl.Matching.Web/Frontend/gulpfile.js
@@ -1,27 +1,53 @@
 ﻿var gulp = require('gulp');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var minify = require('gulp-minify');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 var concatCss = require('gulp-concat-css');
 
-gulp.task('default', ['copy-js', 'copy-css', 'copy-assets']);
+const paths = require('./gulp/paths.json')
+const sassOptions = require('./gulp/sassOptions.js')
 
-gulp.task('copy-js', function () {
+gulp.task('default', ['govuk-js', 'copy-js', 'copy-assets', 'sass', 'merge-css']);
+
+
+gulp.task('govuk-js', () => {
+    gulp.src([
+        'node_modules/govuk-frontend/*.js',
+        'node_modules/govuk-frontend/vendor/**.js',
+        'node_modules/govuk-frontend/components/**/*.js'
+    ])
+        .pipe(gulp.dest('../wwwroot/govuk/javascripts'));
+});
+
+gulp.task('copy-js', ['copy-js-main', 'copy-js-employer']);
+
+gulp.task('copy-js-main', function () {
     return gulp.src([
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/accessible-autocomplete/dist/accessible-autocomplete.min.js',
-        'src/javascripts/*.js'
     ])
-        .pipe(concat('javascripts/all.js'))
-        .pipe(gulp.dest('../wwwroot/'))
+        .pipe(concat('all.js'))
+        .pipe(minify({
+            noSource: true,
+            ext: {
+                min: '.min.js'
+            }
+        }))
+        .pipe(gulp.dest(paths.dist.defaultJs))
 });
 
-gulp.task('copy-css', function () {
-    return gulp.src([
-        'src/stylesheets/*.css', 
-        'node_modules/accessible-autocomplete/dist/accessible-autocomplete.min.css'
-    ])
-        .pipe(concatCss('stylesheets/all.css'))
-        .pipe(gulp.dest('../wwwroot/'))
+gulp.task('copy-js-employer', function () {
+return gulp.src([
+   'src/javascripts/employer-search.js'
+])
+    .pipe(minify({
+        noSource: true,
+        ext: {
+            min: '.min.js'
+        }
+    }))
+    .pipe(gulp.dest(paths.dist.defaultJs))
 });
 
 
@@ -30,3 +56,17 @@ gulp.task('copy-assets', () => {
         .pipe(gulp.dest('../wwwroot/assets'))
 });
 
+gulp.task('sass', () => gulp
+    .src(paths.src.default)
+    .pipe(sass(sassOptions))
+    .pipe(gulp.dest(paths.mid.default)));
+
+gulp.task('merge-css', function () {
+    return gulp.src([
+        'node_modules/accessible-autocomplete/dist/accessible-autocomplete.min.css',
+        "src/stylesheets/css/*.css"
+    ])
+        .pipe(concatCss("main.css"))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('../wwwroot/stylesheets/'));
+})
