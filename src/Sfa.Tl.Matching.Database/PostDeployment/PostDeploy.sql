f4ae7dc7cf54d4d4c3b23b1e3d4b5958e96599f7
Mike Wild <mike.wild@digital.education.gov.uk>
2019-02-27 11:42:53 +0000

    Remove obsolete (renamed) RoutePathMapping table

--- src/Sfa.Tl.Matching.Database/PostDeployment/PostDeploy.sql
+++ src/Sfa.Tl.Matching.Database/PostDeployment/PostDeploy.sql
@@ -4,3 +4,7 @@ Post-Deployment Script
 
 :r ".\Seed Routes.sql"
 :r ".\Seed Paths.sql"
+
+--Remove obsolete tables
+IF EXISTS(SELECT 1 FROM sys.tables WHERE [name] = 'RoutePathMapping')
+	DROP TABLE [dbo].[RoutePathMapping]
