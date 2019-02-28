f66c2c094fb17c9d992bfc28373d68a0c922fada
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-02-28 21:20:02 +0000

    Added PlacementGap table, Entity, ViewModel, View, DbContext, Mapper

---
+++ src/Sfa.Tl.Matching.Database/Tables/ProvisionGap.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProvisionGap]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+	[OpportunityId] INT NOT NULL,
+	[ConfirmationSelected] BIT Null,
+	CONSTRAINT [PK_ProvisionGap] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_ProvisionGap_Opportunity] FOREIGN KEY ([OpportunityId]) REFERENCES [Opportunity]([Id])
+)
