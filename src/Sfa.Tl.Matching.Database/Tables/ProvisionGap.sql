efd57bbd4ef795c5a2919d133d6871c2da709cbd
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-02-28 21:42:06 +0000

    Add Missing Columns to ProvisionGap Table

--- src/Sfa.Tl.Matching.Database/Tables/ProvisionGap.sql
+++ src/Sfa.Tl.Matching.Database/Tables/ProvisionGap.sql
@@ -3,6 +3,10 @@
 	[Id] INT IDENTITY(1,1) NOT NULL, 
 	[OpportunityId] INT NOT NULL,
 	[ConfirmationSelected] BIT Null,
+	[CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
+	[CreatedBy] NVARCHAR(50) NULL, 
+	[ModifiedOn] DATETIME2 NULL, 
+	[ModifiedBy] NVARCHAR(50) NULL
 	CONSTRAINT [PK_ProvisionGap] PRIMARY KEY ([Id]),
 	CONSTRAINT [FK_ProvisionGap_Opportunity] FOREIGN KEY ([OpportunityId]) REFERENCES [Opportunity]([Id])
 )
