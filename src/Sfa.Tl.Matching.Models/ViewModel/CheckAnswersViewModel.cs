58fe4981ca7e02e82cb6e47a6ddad5b1a3be65e8
Adam <adamesfa@outlook.com>
2019-03-04 13:26:31 +0000

    Add required data to ViewModel
    Add confirmation validation
    Update UI with validation

--- src/Sfa.Tl.Matching.Models/ViewModel/CheckAnswersViewModel.cs
+++ src/Sfa.Tl.Matching.Models/ViewModel/CheckAnswersViewModel.cs
@@ -1,9 +1,18 @@
-﻿namespace Sfa.Tl.Matching.Models.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sfa.Tl.Matching.Models.ViewModel
 {
     public class CheckAnswersViewModel
     {
         public int OpportunityId { get; set; }
-
+        public string EmployerName { get; set; }
+        public string Route { get; set; }
+        public string Postcode { get; set; }
+        public short Distance { get; set; }
+        public string JobTitle { get; set; }
+        public short? Placements { get; set; }
+        public string Contact { get; set; }
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must confirm that we can share the employer’s details with the selected providers.")]
         public bool ConfirmationSelected { get; set; }
         public string CreatedBy { get; set; }
     }
