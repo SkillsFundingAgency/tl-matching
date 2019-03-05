e40f5bd1d2099594ecda28c78d6e22566badecbe
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-03-05 14:33:26 +0000

    Added ProviderVenueId to ProviderVenueSearchResultDto
    (cherry picked from commit c8305ac69132acb467461752bf0df91b7b780844)

--- src/Sfa.Tl.Matching.Models/Dto/ProviderVenueSearchResultDto.cs
+++ src/Sfa.Tl.Matching.Models/Dto/ProviderVenueSearchResultDto.cs
@@ -4,10 +4,11 @@ namespace Sfa.Tl.Matching.Models.Dto
 {
     public class ProviderVenueSearchResultDto
     {
+        public int ProviderId { get; set; }
+        public int ProviderVenueId { get; set; }
+        public string ProviderName { get; set; }
         public string Postcode { get; set; }
         public decimal? Distance { get; set; }
-        public int ProviderId { get; set; }
-        public string ProviderName { get; set; }
         public IEnumerable<string> QualificationShortTitles { get; set; }
     }
 }
