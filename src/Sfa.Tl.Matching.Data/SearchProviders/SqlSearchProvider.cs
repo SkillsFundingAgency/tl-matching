928c3cb5ffca9b4943c24661f0fd8b4745184ba7
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-03-05 16:07:37 +0000

    basic query to search ProviderVenue by Postcode and Route
    (cherry picked from commit 537bc16565b7e7928a82d9bfc3b09b2504205830)

--- src/Sfa.Tl.Matching.Data/SearchProviders/SqlSearchProvider.cs
+++ src/Sfa.Tl.Matching.Data/SearchProviders/SqlSearchProvider.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using NetTopologySuite.Geometries;
 using Sfa.Tl.Matching.Data.Interfaces;
 using Sfa.Tl.Matching.Models.Dto;
 
@@ -21,7 +24,19 @@ namespace Sfa.Tl.Matching.Data.SearchProviders
         {
             _logger.LogInformation($"Searching for providers within radius {searchRadius} of postcode '{postcode}' with route {routeId}");
 
-            return await Task.FromResult(new List<ProviderVenueSearchResultDto>());
+            var qualificationIds = _dbContext.QualificationRoutePathMapping.Where(qrpm => qrpm.Path.RouteId == routeId).Select(qrpm => qrpm.QualificationId).Distinct();
+
+            return _dbContext.ProviderVenue.Where(p => p.Location.Distance(new Point(1.2, 1.2)) <= searchRadius && p.ProviderQualification.Select(pq => pq.QualificationId).Any(qId => qualificationIds.Contains(qId)))
+                .Select(p => new ProviderVenueSearchResultDto
+                {
+                    ProviderName = p.Provider.Name,
+                    Distance = decimal.Parse(p.Location.Distance(new Point(1.2, 1.2)).ToString(CultureInfo.InvariantCulture)),
+                    ProviderVenueId = p.Id,
+                    Postcode = p.Postcode,
+                    ProviderId = p.ProviderId,
+                    QualificationShortTitles = p.ProviderQualification.Select(pq => pq.Qualification.ShortTitle)
+                })
+                .OrderBy(dto => dto.Distance);
         }
     }
 }
