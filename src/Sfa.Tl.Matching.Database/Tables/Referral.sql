646348de66d010d764a4c45828deaeb752163783
Adam <adamesfa@outlook.com>
2019-03-06 10:22:24 +0000

    Add Referral table

---
+++ src/Sfa.Tl.Matching.Database/Tables/Referral.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Referral]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+	[OpportunityId] INT NOT NULL,
+	[EmailId] INT NULL,
+	[EmailSent] BIT NOT NULL,
+	[TotalProviders] INT NOT NULL,
+	[CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
+	[CreatedBy] NVARCHAR(50) NULL, 
+	[ModifiedOn] DATETIME2 NULL, 
+	[ModifiedBy] NVARCHAR(50) NULL
+	CONSTRAINT [PK_Referral] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_Referral_Opportunity] FOREIGN KEY ([OpportunityId]) REFERENCES [Opportunity]([Id])
+)
