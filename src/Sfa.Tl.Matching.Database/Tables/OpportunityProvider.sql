3627c68a898dc45e88a97866f326d9d33dc25ed8
Adam <adamesfa@outlook.com>
2019-03-07 14:08:02 +0000

    Add new tables

---
+++ src/Sfa.Tl.Matching.Database/Tables/OpportunityProvider.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[OpportunityProvider]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+	[ReferralId] INT NOT NULL, 
+	[ProviderVenueId] INT NOT NULL,
+	[CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
+	[CreatedBy] NVARCHAR(50) NULL, 
+	[ModifiedOn] DATETIME2 NULL, 
+	[ModifiedBy] NVARCHAR(50) NULL
+	CONSTRAINT [PK_OpportunityProvider] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_OpportunityProvider_ProviderVenue] FOREIGN KEY ([ProviderVenueId]) REFERENCES [ProviderVenue]([Id]),
+	CONSTRAINT [FK_OpportunityProvider_Referral] FOREIGN KEY ([ReferralId]) REFERENCES [Referral]([Id])
+)
