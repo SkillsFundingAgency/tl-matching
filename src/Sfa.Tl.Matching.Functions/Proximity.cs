c6f4a3c3b515bd83d97685fb144c1e3a2ea51505
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-03-08 12:32:35 +0000

    Renamed Location to Proximity
    (cherry picked from commit 98da5aa53a228df4da3fcc3c6d0735db3fd20925)

---
+++ src/Sfa.Tl.Matching.Functions/Proximity.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Data.Interfaces;
+using Sfa.Tl.Matching.Functions.Extensions;
+using Sfa.Tl.Matching.Models.Dto;
+
+namespace Sfa.Tl.Matching.Functions
+{
+    public class Proximity
+    {
+        [FunctionName("GetProximityData")]
+        [return: Queue("save-proximity-queue")]
+        public async Task<SaveProximityData> GetProximityData(
+            [QueueTrigger("get-proximity-queue", Connection = "BlobStorageConnectionString")]GetProximityData getProximityData,
+            ExecutionContext context,
+            ILogger logger,
+            [Inject] IMapper mapper,
+            [Inject]ILocationService locationService
+        )
+        {
+            var saveProximityData = new SaveProximityData { PostCode = getProximityData.PostCode, UkPrn = getProximityData.UkPrn };
+            
+            var geoLocationData = await locationService.GetGeoLocationData(getProximityData.PostCode);
+            
+            return mapper.Map(geoLocationData, saveProximityData);
+        }
+
+        [FunctionName("SaveProximityData")]
+        public async Task SaveProximityData(
+            [QueueTrigger("save-proximity-queue", Connection = "BlobStorageConnectionString")]SaveProximityData saveProximityData,
+            ExecutionContext context,
+            ILogger logger,
+            [Inject]IRepository<Domain.Models.ProviderVenue> providerVenueRepository
+        )
+        {
+            var providerVenue = await providerVenueRepository.GetSingleOrDefault(venue => venue.Provider.UkPrn == saveProximityData.UkPrn && venue.Postcode == saveProximityData.PostCode);
+            if(providerVenue == null) return;
+            
+            providerVenue.Latitude = saveProximityData.Latitude;
+            providerVenue.Longitude = saveProximityData.Longitude;
+            await providerVenueRepository.Update(providerVenue);
+        }
+    }
+}
