903367d1deff458be82ac2254759aebac6352778
Ashley Anderton <ashley.j.anderton@gmail.com>
2019-03-08 13:40:55 +0000

    Added feedback page, progress so far

---
+++ src/Sfa.Tl.Matching.Web/Controllers/FeedbackController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.Matching.Models.ViewModel;
+
+namespace Sfa.Tl.Matching.Web.Controllers
+{
+    [AllowAnonymous]
+    public class FeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
