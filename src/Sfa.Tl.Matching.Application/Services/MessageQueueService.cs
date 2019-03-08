2b09ccaf6e6caeed9ea6add27ef11dfb21eb35c1
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-03-08 14:20:44 +0000

    Added MessageQueueService
    (cherry picked from commit 9c7602daaa0599d94b4fdc657446ab286a603790)

---
+++ src/Sfa.Tl.Matching.Application/Services/MessageQueueService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using Sfa.Tl.Matching.Application.Configuration;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Models.Dto;
+
+namespace Sfa.Tl.Matching.Application.Services
+{
+    public class MessageQueueService : IMessageQueueService
+    {
+        private readonly ILogger<DataBlobUploadService> _logger;
+        private readonly MatchingConfiguration _configuration;
+
+
+        public MessageQueueService(ILogger<DataBlobUploadService> logger, MatchingConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public async Task Push(GetProximityData getProximityData)
+        {
+            var message = JsonConvert.SerializeObject(getProximityData);
+            var queue = await GetProximityQueue();
+            await queue.AddMessageAsync(new CloudQueueMessage(message));
+        }
+
+        private async Task<CloudQueue> GetProximityQueue()
+        {
+            var storageAccount = CloudStorageAccount.Parse(_configuration.BlobStorageConnectionString);
+            var client = storageAccount.CreateCloudQueueClient();
+
+            var queueReference = client.GetQueueReference("get-proximity-queue");
+            await queueReference.CreateIfNotExistsAsync();
+
+            return queueReference;
+        }
+    }
+}
