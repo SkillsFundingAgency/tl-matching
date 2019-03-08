3ac911b5aedc7f18958e4b0128469dc2b79ce13c
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-03-08 12:33:07 +0000

    Added GetProximityData and SaveProximityData
    (cherry picked from commit 7d45985d2c307957e582427beedabaf1abb80936)

---
+++ src/Sfa.Tl.Matching.Models/Dto/SaveProximityData.cs
@@ -0,0 +1,10 @@
+﻿namespace Sfa.Tl.Matching.Models.Dto
+{
+    public class SaveProximityData
+    {
+        public long UkPrn { get; set; }
+        public string PostCode { get; set; }
+        public decimal? Longitude { get; set; }
+        public decimal? Latitude { get; set; }
+    }
+}
