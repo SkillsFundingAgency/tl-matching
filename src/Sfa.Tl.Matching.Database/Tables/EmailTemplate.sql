27ffc0a0f7eb2216fe17a85071f220f2e9658c9e
Mike Wild <mike.wild@digital.education.gov.uk>
2019-03-08 12:00:40 +0000

    Add EmailTemplate and OpportunityEmailPlaceholder

---
+++ src/Sfa.Tl.Matching.Database/Tables/EmailTemplate.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[EmailTemplate]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+	[TemplateName] NVARCHAR(50) NOT NULL, 
+	[TemplateId] NVARCHAR(50) NOT NULL, 
+	[Recipients] [NVARCHAR](MAX) NULL, 
+	[CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
+	[CreatedBy] NVARCHAR(50) NULL, 
+	[ModifiedOn] DATETIME2 NULL, 
+	[ModifiedBy] NVARCHAR(50) NULL, 
+
+    CONSTRAINT [PK_EmailTemplates] PRIMARY KEY ([Id])
+)
---
+++ src/Sfa.Tl.Matching.Database/Tables/OpportunityEmailPlaceholder.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[OpportunityEmailPlaceholder]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+	[OpportunityId] INT NOT NULL, 
+	[EmailTemplateId] INT NOT NULL, 
+	[Key] VARCHAR(50) NOT NULL, 
+	[Value] NVARCHAR(500) NOT NULL,
+	[CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
+	[CreatedBy] NVARCHAR(50) NULL, 
+	[ModifiedOn] DATETIME2 NULL, 
+	[ModifiedBy] NVARCHAR(50) NULL, 
+
+    CONSTRAINT [PK_OpportunityEmailPlaceholder] PRIMARY KEY ([Id]),
+
+	CONSTRAINT [FK_OpportunityEmailPlaceholder_Opportunity] FOREIGN KEY ([OpportunityId]) REFERENCES [Opportunity]([Id]),
+	CONSTRAINT [FK_OpportunityEmailPlaceholder_EmailTemplate] FOREIGN KEY ([EmailTemplateId]) REFERENCES [EmailTemplate]([Id])
+)
