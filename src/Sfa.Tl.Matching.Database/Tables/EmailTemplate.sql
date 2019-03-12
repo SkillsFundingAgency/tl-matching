08d80640f4765265235adc3abb117287c0da61a6
Mike Wild <mike.wild@digital.education.gov.uk>
2019-03-12 07:47:45 +0000

    Fix bad merge in EmailTemplate table

--- src/Sfa.Tl.Matching.Database/Tables/EmailTemplate.sql
+++ src/Sfa.Tl.Matching.Database/Tables/EmailTemplate.sql
@@ -1,10 +1,6 @@
 ﻿CREATE TABLE [dbo].[EmailTemplate]
 (
-<<<<<<< HEAD
-	[Id] INT NOT NULL IDENTITY(1,1), 
-=======
-	[Id] INT IDENTITY(1,1) NOT NULL, 
->>>>>>> master
+	[Id] INT IDENTITY(1,1) NOT NULL,
 	[TemplateName] NVARCHAR(50) NOT NULL, 
 	[TemplateId] NVARCHAR(50) NOT NULL, 
 	[Recipients] [NVARCHAR](MAX) NULL, 
