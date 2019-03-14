9159cd3aeb6003de5dea50fa8b7ce60016e8cbf8
Adam <adamesfa@outlook.com>
2019-03-14 10:02:41 +0000

    Remove Feedback from controller. It is no longer needed.

--- src/Sfa.Tl.Matching.Web/Controllers/HomeController.cs
+++ src/Sfa.Tl.Matching.Web/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Sfa.Tl.Matching.Application.Extensions;
 using Sfa.Tl.Matching.Models.ViewModel;
 
 namespace Sfa.Tl.Matching.Web.Controllers
@@ -40,13 +39,6 @@ namespace Sfa.Tl.Matching.Web.Controllers
         }
 
         public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        // TODO - Decide whats happening with this. Add tests
-        [Authorize(Roles = RolesExtensions.StandardUser + "," + RolesExtensions.AdminUser)]
-        public IActionResult Feedback()
         {
             return View();
         }
