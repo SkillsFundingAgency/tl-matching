ce801b501e636bab7eab21477cc765fd911e67b7
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-03-15 22:10:51 +0000

    Rename BusinessName to CompanyName

--- src/Sfa.Tl.Matching.Models/Dto/EmployerNameDto.cs
+++ src/Sfa.Tl.Matching.Models/Dto/EmployerNameDto.cs
@@ -5,8 +5,8 @@ namespace Sfa.Tl.Matching.Models.Dto
     public class EmployerNameDto
     {
         public int OpportunityId { get; set; }
-        public string CompanyName { get; set; }
         public int EmployerId { get; set; }
+        public string CompanyName { get; set; }
         public DateTime? ModifiedOn { get; set; }
         public string ModifiedBy { get; set; }
     }
