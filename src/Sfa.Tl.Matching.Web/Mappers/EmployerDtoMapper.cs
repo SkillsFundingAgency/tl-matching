54742bb9d4afacf9816541c8869025388d1cfa73
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-03-15 10:02:41 +0000

    Added EmployerDtoMappers

---
+++ src/Sfa.Tl.Matching.Web/Mappers/EmployerDtoMapper.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Sfa.Tl.Matching.Application.Mappers;
+using Sfa.Tl.Matching.Models.Dto;
+using Sfa.Tl.Matching.Models.ViewModel;
+
+namespace Sfa.Tl.Matching.Web.Mappers
+{
+    public class EmployerDtoMapper : Profile
+    {
+        public EmployerDtoMapper()
+        {
+            CreateMap<FindEmployerViewModel, EmployerNameDto>()
+                .ForMember(m => m.ModifiedBy, o => o.MapFrom<LoggedInUserNameResolver<FindEmployerViewModel, EmployerNameDto>>())
+                .ForMember(m => m.ModifiedOn, o => o.MapFrom<UtcNowResolver<FindEmployerViewModel, EmployerNameDto>>())
+                .ForMember(m => m.EmployerId, o => o.MapFrom(s => s.SelectedEmployerId))
+                ;
+
+            CreateMap<EmployerDetailsViewModel, EmployerDetailDto>()
+                .ForMember(m => m.ModifiedBy, o => o.MapFrom<LoggedInUserNameResolver<EmployerDetailsViewModel, EmployerDetailDto>>())
+                .ForMember(m => m.ModifiedOn, o => o.MapFrom<UtcNowResolver<EmployerDetailsViewModel, EmployerDetailDto>>())
+                ;
+
+            CreateMap<EmployerDto, EmployerDetailsViewModel>()
+                .ForMember(m => m.Contact, o => o.MapFrom(s => s.PrimaryContact))
+                .ForMember(m => m.ContactEmail, o => o.MapFrom(s => s.Email))
+                .ForMember(m => m.ContactPhone, o => o.MapFrom(s => s.Phone))
+                ;
+        }
+    }
+}
