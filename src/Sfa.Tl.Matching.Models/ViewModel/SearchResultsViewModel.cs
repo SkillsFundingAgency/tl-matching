d405b8268fc47e39e68d01329dd2c9b6e74ae6fa
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-03-15 22:09:58 +0000

    Added SearchResultProviderCount

--- src/Sfa.Tl.Matching.Models/ViewModel/SearchResultsViewModel.cs
+++ src/Sfa.Tl.Matching.Models/ViewModel/SearchResultsViewModel.cs
@@ -4,6 +4,7 @@ namespace Sfa.Tl.Matching.Models.ViewModel
 {
     public class SearchResultsViewModel
     {
+        public int SearchResultProviderCount => Results?.Count ?? 0;
         public IList<SearchResultsViewModelItem> Results { get; set; }
     }
 }
