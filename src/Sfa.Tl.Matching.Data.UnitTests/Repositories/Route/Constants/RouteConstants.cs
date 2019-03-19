232f62bed3a9b2e4bf548d632682f68c3abcce25
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-03-19 09:58:33 +0000

    Resharper Code Clean-Up

--- src/Sfa.Tl.Matching.Data.UnitTests/Repositories/Route/Constants/RouteConstants.cs
+++ src/Sfa.Tl.Matching.Data.UnitTests/Repositories/Route/Constants/RouteConstants.cs
@@ -3,7 +3,6 @@
     public static class RouteConstants
     {
         public const int Id = 1;
-        public const int Id2 = 2;
         public const string Name = "Route 1";
         public const string SecondName = "Route 2";
         public const string Keywords = "Keyword";
