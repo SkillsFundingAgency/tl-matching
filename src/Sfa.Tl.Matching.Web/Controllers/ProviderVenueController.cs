d2efb7f38667f64b6a93f26f990c290dce9e326b
Adam <adamesfa@outlook.com>
2019-04-11 09:46:45 +0100

    Add View for styling

--- src/Sfa.Tl.Matching.Web/Controllers/ProviderVenueController.cs
+++ src/Sfa.Tl.Matching.Web/Controllers/ProviderVenueController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.Matching.Models.ViewModel;
 
 namespace Sfa.Tl.Matching.Web.Controllers
 {
     public class ProviderVenueController : Controller
     {
-        
+        [Route("overview-{postcode}", Name = "SearchVenue")]
+        public IActionResult ProviderVenueDetail(string postcode)
+        {
+            var viewModel = new ProviderVenueDetailViewModel();
+
+            return View(viewModel);
+        }
     }
 }
