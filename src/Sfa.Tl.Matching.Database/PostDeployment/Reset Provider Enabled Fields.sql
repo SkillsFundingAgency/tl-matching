799411777a8b24b87318c2a06073ba4c1d77a2c2
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-04-15 15:14:14 +0100

    Added PostDeploy Script to set IsEnabledForSearch and IsEnabledForReferral

--- src/Sfa.Tl.Matching.Database/PostDeployment/Reset Provider Enabled Fields.sql
+++ src/Sfa.Tl.Matching.Database/PostDeployment/Reset Provider Enabled Fields.sql
@@ -1,6 +1,5 @@
-﻿UPDATE [dbo].[Provider]
-SET [IsEnabledForSearch]  = 1,
-	[IsEnabledForReferral] = 1
+﻿UPDATE [dbo].[Provider] SET [IsEnabledForSearch]  = 1, [IsEnabledForReferral] = 1;
 
 ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForSearch] BIT NOT NULL;
-ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForReferral] BIT NOT NULL
+
+ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForReferral] BIT NOT NULL;
