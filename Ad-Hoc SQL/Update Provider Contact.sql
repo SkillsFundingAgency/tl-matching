f2832336ecd2b319ed73f09971c1954cdbbe935d
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-04-15 13:32:02 +0100

    Ad-Hoc SQL Update

---
+++ Ad-Hoc SQL/Update Provider Contact.sql
@@ -0,0 +1,115 @@
+UPDATE [dbo].[Provider]
+SET 
+	[PrimaryContact]	  = 'Mrs Maggie Jones'
+	,[PrimaryContactEmail] = 'mjones@arthurmellows.org'
+	,[PrimaryContactPhone] = '01733 252235'
+	,[SecondaryContact]	  = 'Emma Kavanagh'	
+	,[SecondaryContactEmail] = 'ekavanagh@arthurmellows.org'
+	,[SecondaryContactPhone] = '01733 252235'
+WHERE [UkPrn] = '10031362'
+
+
+
+
+UPDATE [dbo].[Provider]
+SET 
+	[PrimaryContact]	  = 'Roy Mason'
+	,[PrimaryContactEmail] = 'masonr@chesterfield.ac.uk'
+	,[PrimaryContactPhone] = '01246 500500 ext 1214'
+	,[SecondaryContact]	  = 'Rachel Greaves'	
+	,[SecondaryContactEmail] = 'greavesr@chesterfield.ac.uk'
+	,[SecondaryContactPhone] = '01246 500500 ext 1211'
+WHERE [UkPrn] = '10001378'
+			
+		
+
+
+UPDATE [dbo].[Provider]
+SET 
+	[PrimaryContact]	  = 'Siobhan Dunne'
+	,[PrimaryContactEmail] = 'sdunne@hrc.ac.uk'
+	,[PrimaryContactPhone] = '01992 411494'
+	,[SecondaryContact]	  = 'Ryan Cowell'	
+	,[SecondaryContactEmail] = 'rcowell@hrc.ac.uk'
+	,[SecondaryContactPhone] = '01992 411494'
+WHERE [UkPrn] = '10003035'
+
+
+
+
+
+UPDATE [dbo].[Provider]
+SET 
+	[PrimaryContact]	  = 'Kirsten Lea'
+	,[PrimaryContactEmail] = 'kirsten.lea@mkcollege.ac.uk'
+	,[PrimaryContactPhone] = '01908 724296'
+	,[SecondaryContact]	  = 'Tracey Matthews'	
+	,[SecondaryContactEmail] = 'tracey.matthews@mkcollege.ac.uk'
+	,[SecondaryContactPhone] = '01908 684576'
+WHERE [UkPrn] = '10004375'
+
+
+
+
+
+UPDATE [dbo].[Provider]
+SET 
+	[PrimaryContact]	  = 'Adam Lea'
+	,[PrimaryContactEmail] = 'adam.lea@ncl-coll.ac.uk'
+	,[PrimaryContactPhone] = '01912 004751'
+	,[SecondaryContact]	  = 'Tom Ferry'	
+	,[SecondaryContactEmail] = 'thomas.ferry@ncl-coll.ac.uk'
+	,[SecondaryContactPhone] = '01912 004751'
+WHERE [UkPrn] = '10004599'
+
+
+
+
+UPDATE [dbo].[Provider]
+SET 
+	[PrimaryContact]	  = 'Danielle Glenton'
+	,[PrimaryContactEmail] = 'danielle.glenton@northland.ac.uk'
+	,[PrimaryContactPhone] = '01670 841200 ext 2397'
+	,[SecondaryContact]	  = 'Jen Bell'	
+	,[SecondaryContactEmail] = 'jen.bell@northland.ac.uk'
+	,[SecondaryContactPhone] = '01670 841200'
+WHERE [UkPrn] = '10004760'			
+
+
+
+
+UPDATE [dbo].[Provider]
+SET 
+	[PrimaryContact]	  = 'Placements team'
+	,[PrimaryContactEmail] = 'industry.placement@suffolkone.ac.uk'
+	,[PrimaryContactPhone] = '01473 556600'
+	,[SecondaryContact]	  = 'Michelle Hughes'	
+	,[SecondaryContactEmail] = 'michelle.hughes@suffolkone.ac.uk'
+	,[SecondaryContactPhone] = '01473 556600'
+WHERE [UkPrn] = '10053874'			
+
+
+
+
+UPDATE [dbo].[Provider]
+SET 
+	[PrimaryContact]	  = 'Sarah Birbeck'
+	,[PrimaryContactEmail] = 'sarah.birbeck@southampton-city.ac.uk'
+	,[PrimaryContactPhone] = '023 80 577279'
+	,[SecondaryContact]	  = 'Lorraine Wheeler'	
+	,[SecondaryContactEmail] = 'lorraine.wheeler@southampton-city.ac.uk'
+	,[SecondaryContactPhone] = '02380 577326'
+WHERE [UkPrn] = '10006020'			
+
+
+
+
+UPDATE [dbo].[Provider]
+SET 
+	[PrimaryContact]	  = 'Nick Gribben'
+	,[PrimaryContactEmail] = 'ngribben@sthelens.ac.uk'
+	,[PrimaryContactPhone] = '01744 623111'
+	,[SecondaryContact]	  = 'Jette Burford'	
+	,[SecondaryContactEmail] = 'jburford@sthelens.ac.uk'
+	,[SecondaryContactPhone] = '01744 623351'
+WHERE [UkPrn] = '10006174'			
