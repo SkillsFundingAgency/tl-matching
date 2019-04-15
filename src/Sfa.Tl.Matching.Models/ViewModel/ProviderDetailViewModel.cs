fff6d7901c202d564b567c2569d942e71d242f1a
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-04-15 13:48:22 +0100

    Added new fields in Provider and new ViewModels

--- src/Sfa.Tl.Matching.Models/ViewModel/ProviderDetailViewModel.cs
+++ src/Sfa.Tl.Matching.Models/ViewModel/ProviderDetailViewModel.cs
@@ -1,24 +1,23 @@
 ﻿using System.Collections.Generic;
 
-namespace Sfa.Tl.Matching.Domain.Models
+namespace Sfa.Tl.Matching.Models.ViewModel
 {
-    public class Provider : BaseEntity
+    public class ProviderDetailViewModel
     {
+        public int Id { get; set; }
         public long UkPrn { get; set; }
         public string Name { get; set; }
         public string DisplayName { get; set; }
-        public int OfstedRating { get; set; }
-        public bool Status { get; set; }
-        public string StatusReason { get; set; }
         public string PrimaryContact { get; set; }
         public string PrimaryContactEmail { get; set; }
         public string PrimaryContactPhone { get; set; }
         public string SecondaryContact { get; set; }
         public string SecondaryContactEmail { get; set; }
         public string SecondaryContactPhone { get; set; }
+
         public bool IsEnabledForReferral { get; set; }
         public bool IsEnabledForSearch { get; set; }
-        public string Source { get; set; }
-        public virtual ICollection<ProviderVenue> ProviderVenue { get; set; }
+
+        public List<ProviderVenueViewModel> ProviderVenues { get; set; }
     }
 }
---
+++ src/Sfa.Tl.Matching.Models/ViewModel/ProviderVenueViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Sfa.Tl.Matching.Models.ViewModel
+{
+    public class ProviderVenueViewModel
+    {
+        public string Postcode { get; set; }
+        public int QualificationCount { get; set; }
+        public int ProviderVenueId { get; set; }
+        public bool IsEnabledForSearch { get; set; }
+    }
+}
