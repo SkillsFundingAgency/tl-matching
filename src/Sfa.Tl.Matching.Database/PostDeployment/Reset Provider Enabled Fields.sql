0cf7d91ffd574c7cbfe6117b9fed9c409f6b21c7
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-04-15 14:01:49 +0100

    Added Post Deployment script to backfill IsEnabledForSearch  and IsEnabledForReferral

---
+++ src/Sfa.Tl.Matching.Database/PostDeployment/Reset Provider Enabled Fields.sql
@@ -0,0 +1,6 @@
+﻿UPDATE [dbo].[Provider]
+SET [IsEnabledForSearch]  = 1,
+	[IsEnabledForReferral] = 1
+
+ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForSearch] BIT NOT NULL;
+ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForReferral] BIT NOT NULL
