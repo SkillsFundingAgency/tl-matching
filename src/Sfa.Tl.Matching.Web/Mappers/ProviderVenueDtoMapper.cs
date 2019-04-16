88f7d40548be1db37d938251875406f216d13220
Adam <adamesfa@outlook.com>
2019-04-16 10:01:24 +0100

    Add Id for updates

--- src/Sfa.Tl.Matching.Web/Mappers/ProviderVenueDtoMapper.cs
+++ src/Sfa.Tl.Matching.Web/Mappers/ProviderVenueDtoMapper.cs
@@ -21,6 +21,7 @@ namespace Sfa.Tl.Matching.Web.Mappers
                 .ForMember(m => m.ModifiedBy, o => o.MapFrom<LoggedInUserNameResolver<ProviderVenueDetailViewModel, UpdateProviderVenueDto>>())
                 .ForMember(m => m.ModifiedOn, o => o.MapFrom<UtcNowResolver<ProviderVenueDetailViewModel, UpdateProviderVenueDto>>())
                 .ForMember(m => m.Name, o => o.MapFrom(s => s.VenueName))
+                .ForMember(m => m.Id, o => o.MapFrom(s => s.Id))
                 .ForMember(m => m.ProviderId, o => o.MapFrom(s => s.ProviderId))
                 .ForMember(m => m.Postcode, o => o.MapFrom(s => s.Postcode))
                 .ForMember(m => m.Source, o => o.MapFrom(s => s.Source))
