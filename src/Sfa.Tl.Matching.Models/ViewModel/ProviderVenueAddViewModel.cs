ec72cc54334dfd417be5564a2439fb1c8d712633
Adam <adamesfa@outlook.com>
2019-04-16 09:00:49 +0100

    Add Source

--- src/Sfa.Tl.Matching.Models/ViewModel/ProviderVenueAddViewModel.cs
+++ src/Sfa.Tl.Matching.Models/ViewModel/ProviderVenueAddViewModel.cs
@@ -8,6 +8,6 @@ namespace Sfa.Tl.Matching.Models.ViewModel
         public long UkPrn { get; set; }
         [Required(ErrorMessage = "You must enter a postcode")]
         public string Postcode { get; set; }
-        public string Source { get; set; } = string.Empty;
+        public string Source { get; set; }
     }
 }
