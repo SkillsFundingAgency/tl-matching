9e01eb99aa21d3fadd4b712d35b8d80ae01434e9
Adam <adamesfa@outlook.com>
2019-04-17 10:09:03 +0100

    Remove Reset Provider Enabled Fields.sql as its already in PostDeploy.sql

--- src/Sfa.Tl.Matching.Database/PostDeployment/Reset Provider Enabled Fields.sql
+++
@@ -1,5 +0,0 @@
-﻿UPDATE [dbo].[Provider] SET [IsEnabledForSearch]  = 1, [IsEnabledForReferral] = 1;
-
-ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForSearch] BIT NOT NULL;
-
-ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForReferral] BIT NOT NULL;
