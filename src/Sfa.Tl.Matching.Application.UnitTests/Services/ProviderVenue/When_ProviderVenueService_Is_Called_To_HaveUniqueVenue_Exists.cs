f21d0b753165c9cab863a99ce847c6d81175e481
Adam <adamesfa@outlook.com>
2019-04-17 12:35:42 +0100

    Add ProviderVenue service tests

---
+++ src/Sfa.Tl.Matching.Application.UnitTests/Services/ProviderVenue/When_ProviderVenueService_Is_Called_To_HaveUniqueVenue_Exists.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Application.Mappers;
+using Sfa.Tl.Matching.Application.Services;
+using Sfa.Tl.Matching.Data.Interfaces;
+using Sfa.Tl.Matching.Domain.Models;
+using Xunit;
+
+namespace Sfa.Tl.Matching.Application.UnitTests.Services.Provider
+{
+    public class When_ProviderVenueService_Is_Called_To_HaveUniqueVenue_Exists
+    {
+        private readonly IRepository<ProviderVenue> _providerVenueRepository;
+        private readonly bool _isUniqueVenue;
+        private const long UkPrn = 123;
+        private const string Postcode = "CV1 2WT";
+
+        public When_ProviderVenueService_Is_Called_To_HaveUniqueVenue_Exists()
+        {
+            var config = new MapperConfiguration(c => c.AddProfiles(typeof(ProviderMapper).Assembly));
+            var mapper = new Mapper(config);
+            _providerVenueRepository = Substitute.For<IProviderVenueRepository>();
+
+            _providerVenueRepository.GetSingleOrDefault(Arg.Any<Expression<Func<ProviderVenue, bool>>>())
+                .Returns(new ProviderVenue());
+
+            var locationService = Substitute.For<ILocationService>();
+            var providerVenueService = new ProviderVenueService(mapper, _providerVenueRepository,
+                locationService);
+
+            _isUniqueVenue = providerVenueService.HaveUniqueVenueAsync(UkPrn, Postcode).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public void Then_ProviderVenueRepository_GetSingleOrDefault_Is_Called_Exactly_Once()
+        {
+            _providerVenueRepository.Received(1).GetSingleOrDefault(Arg.Any<Expression<Func<ProviderVenue, bool>>>());
+        }
+
+        [Fact]
+        public void Then_IsUniqueVenue_Is_False()
+        {
+            _isUniqueVenue.Should().BeFalse();
+        }
+    }
+}
---
+++ src/Sfa.Tl.Matching.Application.UnitTests/Services/ProviderVenue/When_ProviderVenueService_Is_Called_To_UpdateVenue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq.Expressions;
+using AutoMapper;
+using NSubstitute;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Application.Mappers;
+using Sfa.Tl.Matching.Application.Services;
+using Sfa.Tl.Matching.Data.Interfaces;
+using Sfa.Tl.Matching.Domain.Models;
+using Sfa.Tl.Matching.Models.Dto;
+using Xunit;
+
+namespace Sfa.Tl.Matching.Application.UnitTests.Services.Provider
+{
+    public class When_ProviderVenueService_Is_Called_To_UpdateVenue
+    {
+        private readonly IProviderVenueRepository _providerVenueRepository;
+
+        public When_ProviderVenueService_Is_Called_To_UpdateVenue()
+        {
+            var config = new MapperConfiguration(c => c.AddProfiles(typeof(ProviderMapper).Assembly));
+            var mapper = new Mapper(config);
+            _providerVenueRepository = Substitute.For<IProviderVenueRepository>();
+
+            _providerVenueRepository.GetSingleOrDefault(Arg.Any<Expression<Func<ProviderVenue, bool>>>())
+                .Returns(new ProviderVenue());
+
+            var locationService = Substitute.For<ILocationService>();
+            var providerVenueService = new ProviderVenueService(mapper, _providerVenueRepository,
+                locationService);
+
+            providerVenueService.UpdateVenueAsync(new UpdateProviderVenueDto()).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public void Then_ProviderVenueRepository_GetSingleOrDefault_Is_Called_Exactly_Once()
+        {
+            _providerVenueRepository.Received(1).GetSingleOrDefault(Arg.Any<Expression<Func<ProviderVenue, bool>>>());
+        }
+
+        [Fact]
+        public void Then_ProviderVenueRepository_Update_Is_Called_Exactly_Once()
+        {
+            _providerVenueRepository.Received(1).Update(Arg.Any<ProviderVenue>());
+        }
+    }
+}
