2fee373527e97e2398b71e66d5c265f12687491d
Mike Wild <mike.wild@digital.education.gov.uk>
2019-04-17 11:26:23 +0100

    Move Provider DB post-deployment to separate file

---
+++ src/Sfa.Tl.Matching.Database/PostDeployment/ResetProviderEnabledFields.sql
@@ -0,0 +1,11 @@
+﻿UPDATE [dbo].[Provider]
+SET [IsEnabledForSearch]  = [Status],
+    [IsEnabledForReferral] = [Status]
+
+ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForSearch] BIT NOT NULL;
+ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForReferral] BIT NOT NULL;
+
+UPDATE [dbo].[ProviderVenue]
+SET [IsEnabledForSearch]  = 1
+
+ALTER TABLE [dbo].[ProviderVenue] ALTER COLUMN [IsEnabledForSearch] BIT NOT NULL;
