72704d3d22dacfa0923214933cab74125f0d1a51
Mike Wild <mike.wild@digital.education.gov.uk>
2019-04-17 11:26:23 +0100

    Move Provider DB post-deployment to separate file

--- src/Sfa.Tl.Matching.Database/PostDeployment/PostDeploy.sql
+++ src/Sfa.Tl.Matching.Database/PostDeployment/PostDeploy.sql
@@ -6,14 +6,5 @@ Post-Deployment Script
 :r ".\Seed Paths.sql"
 :r ".\Seed Email Templates.sql"
 
-UPDATE [dbo].[Provider]
-SET [IsEnabledForSearch]  = [Status],
-    [IsEnabledForReferral] = [Status]
-
-ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForSearch] BIT NOT NULL;
-ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsEnabledForReferral] BIT NOT NULL;
-
-UPDATE [dbo].[ProviderVenue]
-SET [IsEnabledForSearch]  = 1
-
-ALTER TABLE [dbo].[ProviderVenue] ALTER COLUMN [IsEnabledForSearch] BIT NOT NULL;
+--TODO: Remove this file and set fields to NOT NULL in Provider and ProviderVenue (after deployed to live)
+:r ".\ResetProviderEnabledFields.sql"
