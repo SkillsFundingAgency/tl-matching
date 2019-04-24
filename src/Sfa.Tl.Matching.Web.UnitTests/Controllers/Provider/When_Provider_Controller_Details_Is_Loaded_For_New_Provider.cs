f53834cf4d881c65f717cc4c25edda84501cb129
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-04-24 10:12:22 +0100

    Added Test for Provider Edit

---
+++ src/Sfa.Tl.Matching.Web.UnitTests/Controllers/Provider/When_Provider_Controller_Details_Is_Loaded_For_New_Provider.cs
@@ -0,0 +1,53 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Models.ViewModel;
+using Sfa.Tl.Matching.Web.Controllers;
+using Xunit;
+
+namespace Sfa.Tl.Matching.Web.UnitTests.Controllers.Provider
+{
+    public class When_Provider_Controller_GetProviderDetail_Is_Loaded_For_New_Provider
+    {
+        private readonly IActionResult _result;
+        private readonly IProviderService _providerService;
+
+        public When_Provider_Controller_GetProviderDetail_Is_Loaded_For_New_Provider()
+        {
+            _providerService = Substitute.For<IProviderService>();
+
+            var providerController = new ProviderController(_providerService);
+
+            _result = providerController.ProviderDetail(0).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public void Then_Result_Is_Not_Null() =>
+            _result.Should().NotBeNull();
+
+        [Fact]
+        public void Then_View_Result_Is_Returned() =>
+            _result.Should().BeAssignableTo<ViewResult>();
+
+        [Fact]
+        public void Then_Model_Is_Not_Null()
+        {
+            var viewResult = _result as ViewResult;
+            viewResult?.Model.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Then_Model_Is_Of_Type_ProviderDetailViewModel()
+        {
+            var viewResult = _result as ViewResult;
+            viewResult?.Model.Should().BeOfType<ProviderDetailViewModel>();
+        }
+
+        [Fact]
+        public void Then_ProviderService_GetProviderDetailByIdAsync_Is_NOT_Called()
+        {
+            _providerService.DidNotReceive().GetProviderDetailByIdAsync(Arg.Any<int>(), Arg.Any<bool>());
+        }
+    }
+}
