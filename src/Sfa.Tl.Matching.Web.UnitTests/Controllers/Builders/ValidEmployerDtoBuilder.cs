3951753ac4a9072f81bd0b8678a9de8acf20be07
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-04-25 15:51:10 +0100

    Fix Tests as per the new behaviours

--- src/Sfa.Tl.Matching.Web.UnitTests/Controllers/Builders/ValidEmployerDtoBuilder.cs
+++ src/Sfa.Tl.Matching.Web.UnitTests/Controllers/Builders/ValidEmployerDtoBuilder.cs
@@ -8,7 +8,7 @@ namespace Sfa.Tl.Matching.Web.UnitTests.Controllers.Builders
         public EmployerDto Build() => new EmployerDto
         {
             CrmId = new Guid("D7A48843-44CA-46A4-A391-70D7B01C68BC"),
-            CompanyName = "CompanyName",
+            CompanyName = "EmployerName",
             Aupa = "EmployerAupa",
             Owner = "EmployerOwner",
             PrimaryContact = "EmployerContact",
