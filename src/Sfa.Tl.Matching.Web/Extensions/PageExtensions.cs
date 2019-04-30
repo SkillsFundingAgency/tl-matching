5ac624164ca63af6ee943e0e3f4c7279d2e2442d
Adam <adamesfa@outlook.com>
2019-04-30 08:46:19 +0100

    DAC testing. Fix page titles

---
+++ src/Sfa.Tl.Matching.Web/Extensions/PageExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Sfa.Tl.Matching.Web.Extensions
+{
+    public class PageExtensions
+    {
+        private const string ServiceName = "Match employers with providers for industry placements";
+
+        public static string GenerateTitle(bool isValid, string title)
+        {
+            var ignoreTitle = title == ServiceName;
+            var titleSuffix = GetTitleSuffix();
+
+            var formattedTitle = new StringBuilder();
+            if (!isValid) formattedTitle.Append("Error: ");
+            if (string.IsNullOrEmpty(title))
+                return formattedTitle.Append(titleSuffix).ToString();
+
+            if (!ignoreTitle) formattedTitle.Append($"{title} - ");
+            formattedTitle.Append(titleSuffix);
+
+            return formattedTitle.ToString();
+        }
+
+        private static string GetTitleSuffix()
+        {
+            const string govUk = "GOV.UK";
+
+            return $"{ServiceName} - {govUk}";
+        }
+    }
+}
