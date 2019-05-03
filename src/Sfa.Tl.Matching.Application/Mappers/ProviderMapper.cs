916cbc2558541ae3050e361884bde0abb65fe5b5
Mike Wild <wildconsultingltd@googlemail.com>
2019-05-03 11:43:35 +0100

    Remove redundant debugging comment

--- src/Sfa.Tl.Matching.Application/Mappers/ProviderMapper.cs
+++ src/Sfa.Tl.Matching.Application/Mappers/ProviderMapper.cs
@@ -44,22 +44,6 @@ namespace Sfa.Tl.Matching.Application.Mappers
                 .ForMember(m => m.ProviderId, config => config.MapFrom(s => s.Id))
                 .ForMember(m => m.ProviderName, config => config.MapFrom(s => s.Name));
 
-            /*
-            Message: AutoMapper.AutoMapperMappingException : Error mapping types.
-
-Mapping types:
-HideProviderViewModel -> Provider
-Sfa.Tl.Matching.Models.ViewModel.HideProviderViewModel -> Sfa.Tl.Matching.Domain.Models.Provider
-
-Type Map configuration:
-HideProviderViewModel -> Provider
-Sfa.Tl.Matching.Models.ViewModel.HideProviderViewModel -> Sfa.Tl.Matching.Domain.Models.Provider
-
-Destination Member:
-ModifiedBy
-
----- System.MissingMethodException : No parameterless constructor defined for this object.
-             */
             CreateMap<HideProviderViewModel, Provider>()
                 .ForMember(m => m.Id, config => config.MapFrom(s => s.ProviderId))
                 .ForMember(m => m.IsEnabledForSearch, config => config.MapFrom(s => s.IsEnabledForSearch))
