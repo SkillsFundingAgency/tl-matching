f1f2505e373b0f3292269cf655afee561a8ccfec
Mike Wild <wildconsultingltd@googlemail.com>
2019-05-08 16:24:52 +0100

    Update database tables
    Add pre-deploy script to be run manually
    Remove obsolete post-deployment
    
    In Provider, -Rename IsEnabledForSearch to IsCDFProvider,
      drop IsFundedForNextYear
      Also set Secondary Contacts to NULL
    
    In ProviderVenue, Rename IsEnabledForSearch to IsRemoved,
      add and set values for IsEnabledForReferral

--- src/Sfa.Tl.Matching.Database/PostDeployment/ResetProviderEnabledFields.sql
+++
@@ -1,8 +0,0 @@
-﻿--TODO: After IsFundedForNextYear is released to live, 
---      set the column in the table to NOT NULL and remove this file
-
-UPDATE [dbo].[Provider]
-SET [IsFundedForNextYear] = [Status]
-WHERE [IsFundedForNextYear] IS NULL
-
-ALTER TABLE [dbo].[Provider] ALTER COLUMN [IsFundedForNextYear] BIT NOT NULL;
---
+++ src/Sfa.Tl.Matching.Database/PreDeployment/Spreint 8 Predeployment Script.sql
@@ -0,0 +1,42 @@
+﻿/*
+*  One-off script to be run before deploying Sprint 8 release
+*/
+
+--Provider Table Change
+
+--Rename IsEnabledForSearch to IsCDFProvider
+--IsEnabledForSearch = IsFundedForNextYear = New Column => IsCDFProvider
+
+--Make Secondary Contact details Optional (Will be done as a change on the table in the DB project)
+
+if exists (select 1 from sys.columns where name = 'IsEnabledForSearch' and object_name(object_id) = 'Provider') AND
+not exists (select 1 from sys.columns where name = 'IsCDFProvider' and object_name(object_id) = 'Provider')
+	EXEC sp_rename 'Provider.IsEnabledForSearch', 'IsCDFProvider', 'COLUMN';
+GO
+
+if exists (select 1 from sys.columns where name = 'IsFundedForNextYear' and object_name(object_id) = 'Provider')
+	ALTER TABLE [dbo].[Provider] DROP COLUMN [IsFundedForNextYear]
+GO
+
+--Venue Table Changes
+
+--Rename IsEnabledForSearch to IsRemoved
+--New Column => IsEnabledForReferral
+
+if  exists (select 1 from sys.columns where name = 'IsEnabledForSearch' and object_name(object_id) = 'ProviderVenue') AND
+not exists (select 1 from sys.columns where name = 'IsRemoved' and object_name(object_id) = 'ProviderVenue')
+	EXEC sp_rename 'ProviderVenue.IsEnabledForSearch', 'IsRemoved', 'COLUMN';
+GO
+
+if not exists (select 1 from sys.columns where name = 'IsEnabledForReferral' and object_name(object_id) = 'ProviderVenue')
+	ALTER TABLE [dbo].[ProviderVenue] ADD [IsEnabledForReferral] BIT NULL
+GO
+
+UPDATE [dbo].[ProviderVenue]
+SET [IsEnabledForReferral] = [IsRemoved]
+WHERE [IsEnabledForReferral] IS NULL
+
+GO
+ALTER TABLE [dbo].[ProviderVenue] ALTER COLUMN [IsEnabledForReferral] BIT NOT NULL
+GO
+
