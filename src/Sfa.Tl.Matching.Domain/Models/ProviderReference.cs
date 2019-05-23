eccc881988963eab495d67105510ef5586849d48
Adam <adamesfa@outlook.com>
2019-05-23 14:50:38 +0100

    Add Provider Reference tables and models

---
+++ src/Sfa.Tl.Matching.Domain/Models/ProviderReference.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sfa.Tl.Matching.Domain.Models
+{
+    public class ProviderReference : BaseEntity
+    {
+        public long UkPrn { get; set; }
+        public string Name { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed),]
+        public int ChecksumCol { get; set; }
+    }
+}
