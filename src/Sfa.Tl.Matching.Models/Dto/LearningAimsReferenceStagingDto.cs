fcbf418e3edd893695b9f327620cc3c328ffeadf
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-05-23 12:27:22 +0100

    Rename LearningAimsReferenceStaging

---
+++ src/Sfa.Tl.Matching.Models/Dto/LearningAimsReferenceStagingDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sfa.Tl.Matching.Models.Dto
+{
+    public class LearningAimsReferenceStagingDto
+    {
+        public string LearnAimRef { get; set; }
+        public string LearnAimRefTitle { get; set; }
+        public string AwardOrgAimRef { get; set; }
+        public DateTime SourceCreatedOn { get; set; }
+        public DateTime SourceModifiedOn { get; set; }
+    }
+}
