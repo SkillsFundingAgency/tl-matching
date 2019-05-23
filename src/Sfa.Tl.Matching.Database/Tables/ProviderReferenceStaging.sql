9bab3065ec769d241464255f5ad78266a0ee1cef
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-05-23 17:56:37 +0100

    Added Created and Modified column in ProviderReferenceStaging

--- src/Sfa.Tl.Matching.Database/Tables/ProviderReferenceStaging.sql
+++ src/Sfa.Tl.Matching.Database/Tables/ProviderReferenceStaging.sql
@@ -2,6 +2,10 @@
 (
 	[UkPrn] BIGINT NOT NULL,
 	[Name] NVARCHAR(400) NOT NULL,
-	ChecksumCol AS BINARY_CHECKSUM([UkPrn], [Name])
+	[CreatedOn] DATETIME2 NOT NULL DEFAULT GetUTCDate(), 
+	[CreatedBy] NVARCHAR(50) NULL, 
+	[ModifiedOn] DATETIME2 NULL, 
+	[ModifiedBy] NVARCHAR(50) NULL,
+	[ChecksumCol] AS BINARY_CHECKSUM([UkPrn], [Name])
 	CONSTRAINT [PK_ProviderReferenceStaging] PRIMARY KEY ([UkPrn]),
 )
