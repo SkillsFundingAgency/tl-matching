aaed96547132fb7fd4c1f9ba83300d8d80a509ef
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-05-23 15:58:32 +0100

    Fixed Column Names in LearningAimsReferenceStaging Table

--- src/Sfa.Tl.Matching.Domain/Models/LearningAimsReferenceStaging.cs
+++ src/Sfa.Tl.Matching.Domain/Models/LearningAimsReferenceStaging.cs
@@ -4,9 +4,9 @@ namespace Sfa.Tl.Matching.Domain.Models
 {
     public class LearningAimsReferenceStaging : BaseEntity
     {
-        public string LearnAimRef { get; set; } 
-        public string LearnAimRefTitle { get; set; }
-        public string AwardOrgAimRef { get; set; }
+        public string LarId { get; set; } 
+        public string Title { get; set; }
+        public string AwardOrgLarId { get; set; }
         public string SourceCreatedOn { get; set; }
         public string SourceModifiedOn { get; set; }
         
