28e078b30a41fc4cc26e94746c210a4cf8b95ef7
Adam <adamesfa@outlook.com>
2019-05-23 14:50:08 +0100

    Add UkRlp Client

---
+++ src/Sfa.Tl.Matching.UkRlp.Api.Client/ProviderDownload.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Humanizer;
+using Microsoft.Extensions.Logging;
+
+namespace Sfa.Tl.Matching.UkRlp.Api.Client
+{
+    public class ProviderDownload
+    {
+        private readonly ILogger<ProviderDownload> _logger;
+        private readonly TimeSpan _fiveMinuteTimeSpan = new TimeSpan(0, 5, 0);
+
+        public ProviderDownload(ILogger<ProviderDownload> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<List<ProviderRecordStructure>> GetAll(DateTime lastUpdateDate)
+        {
+            var results = new List<ProviderRecordStructure>();
+
+            var query = GetQuery(lastUpdateDate);
+            var client = GetClient();
+
+            _logger.LogInformation("Downloading providers from UKRLP service...");
+
+            var response = await client.retrieveAllProvidersAsync(query);
+
+            _logger.LogInformation($"UKRLP service returned {response.ProviderQueryResponse.MatchingProviderRecords.LongLength} providers");
+            results.AddRange(response.ProviderQueryResponse.MatchingProviderRecords);
+
+            return results;
+        }
+        
+        private ProviderQueryPortTypeClient GetClient()
+        {
+            var client = new ProviderQueryPortTypeClient();
+            client.Endpoint.Binding.SendTimeout = _fiveMinuteTimeSpan;
+            client.Endpoint.Binding.ReceiveTimeout = _fiveMinuteTimeSpan;
+
+            return client;
+        }
+
+        private static ProviderQueryStructure GetQuery(DateTime dtLastUpdate)
+        {
+            var query = new ProviderQueryStructure
+            {
+                QueryId = "1",
+                SelectionCriteria = new SelectionCriteriaStructure
+                {
+                    StakeholderId = "1",
+                    ProviderUpdatedSince = dtLastUpdate,
+                    ProviderUpdatedSinceSpecified = true,
+                    ApprovedProvidersOnly = YesNoType.No,
+                    ApprovedProvidersOnlySpecified = true,
+                    CriteriaCondition = QueryCriteriaConditionType.OR,
+                    CriteriaConditionSpecified = true,
+                    ProviderStatus = UkRlpRecordStatus.Active.Humanize()
+                }
+            };
+
+            return query;
+        }
+    }
+}
---
+++ src/Sfa.Tl.Matching.UkRlp.Api.Client/UkRlpRecordStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Sfa.Tl.Matching.UkRlp.Api.Client
+{
+    public enum UkRlpRecordStatus
+    {
+        [Description("A")]
+        Active,
+        [Description("V")]
+        Verified,
+        [Description("PD1")]
+        DeactivationInProcess,
+        [Description("PD2")]
+        DeactivateComplete
+    }
+}
