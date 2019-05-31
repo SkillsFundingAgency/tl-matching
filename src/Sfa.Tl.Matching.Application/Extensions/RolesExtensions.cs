aa4bb597e4fc15a663b96538314913fd3c4c2faa
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-05-31 14:09:01 +0100

    Use Default values for no Auth Build configuration

--- src/Sfa.Tl.Matching.Application/Extensions/RolesExtensions.cs
+++ src/Sfa.Tl.Matching.Application/Extensions/RolesExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿// ReSharper disable once RedundantUsingDirective
+using System.Linq;
 using System.Security.Claims;
 
 namespace Sfa.Tl.Matching.Application.Extensions
@@ -11,36 +12,51 @@ namespace Sfa.Tl.Matching.Application.Extensions
 
         public static bool HasValidRole(this ClaimsPrincipal user)
         {
+#if NOAUTH
+            return true;
+#else
             return user.IsInRole(StandardUser)
                    || user.IsInRole(AdminUser);
+#endif        
         }
 
         public static bool HasAdminRole(this ClaimsPrincipal user)
         {
+#if NOAUTH
+            return true;
+#else
             return user.IsInRole(AdminUser);
-        }
-
-        public static bool HasStandardRole(this ClaimsPrincipal user)
-        {
-            return user.IsInRole(StandardUser);
+#endif        
         }
 
         public static bool IsAuthorisedAdminUser(this ClaimsPrincipal user, string authorisedUserEmail)
         {
-            return HasAdminRole(user) 
+#if NOAUTH
+            return true;
+#else
+            return HasAdminRole(user)
                 && GetUserEmail(user) == authorisedUserEmail;
+#endif        
         }
 
         public static string GetUserName(this ClaimsPrincipal user)
         {
+#if NOAUTH
+            return "No Auth Admin";
+#else
             var userNames = user.Claims.Where(c => c.Type == ClaimTypes.GivenName || c.Type == ClaimTypes.Surname)
                     .Select(c => c.Value);
             return string.Join(" ", userNames);
+#endif
         }
 
         public static string GetUserEmail(this ClaimsPrincipal user)
         {
+#if NOAUTH
+            return "No.Auth@Admin.com";
+#else
             return user.Claims.SingleOrDefault(c => c.Type == ClaimTypes.Upn)?.Value;
+#endif
         }
     }
 }
