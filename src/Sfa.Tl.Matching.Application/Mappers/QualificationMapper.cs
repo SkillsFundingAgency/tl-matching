91a3896a0839f1ca7264844d00786cffc916cea0
Adam <adamesfa@outlook.com>
2019-06-06 12:21:54 +0100

    Fix qualification mappings

--- src/Sfa.Tl.Matching.Application/Mappers/QualificationMapper.cs
+++ src/Sfa.Tl.Matching.Application/Mappers/QualificationMapper.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿using System.Text;
+using AutoMapper;
+using Sfa.Tl.Matching.Application.Extensions;
 using Sfa.Tl.Matching.Application.Mappers.Resolver;
 using Sfa.Tl.Matching.Domain.Models;
 using Sfa.Tl.Matching.Models.Dto;
@@ -12,6 +14,12 @@ namespace Sfa.Tl.Matching.Application.Mappers
         {
             CreateMap<QualificationDto, Qualification>()
                 .ForMember(m => m.Id, config => config.Ignore())
+
+                .ForMember(m => m.QualificationSearch, config => config.MapFrom(s => GetSearchTerm(s.Title, s.ShortTitle)))
+                .ForMember(m => m.ShortTitleSearch, config => config.MapFrom(s => GetSearchTerm(s.ShortTitle)))
+                //.ForMember(m => m.QualificationSearch, config => config.MapFrom(s => GetSearchTerm(s.Title, s.ShortTitle)))
+                //.ForMember(m => m.ShortTitleSearch, config => config.MapFrom(s => GetSearchTerm(s.ShortTitle)))
+
                 .ForMember(m => m.ProviderQualification, config => config.Ignore())
                 .ForMember(m => m.QualificationRoutePathMapping, config => config.Ignore())
                 .ForMember(m => m.CreatedOn, config => config.Ignore())
@@ -24,6 +32,12 @@ namespace Sfa.Tl.Matching.Application.Mappers
                 .ForMember(m => m.LarsId, config => config.MapFrom(s => s.LarId))
                 .ForMember(m => m.Title, config => config.Ignore())
                 .ForMember(m => m.ShortTitle, config => config.Ignore())
+                .ForMember(m => m.QualificationSearch, config => config.Ignore())
+                .ForMember(m => m.ShortTitleSearch, config => config.Ignore())
+
+                //.ForMember(m => m.QualificationSearch, config => config.MapFrom(s => GetSearchTerm(s.Title, s.ShortTitle)))
+                //.ForMember(m => m.ShortTitleSearch, config => config.MapFrom(s => GetSearchTerm(s.ShortTitle)))
+
                 .ForMember(m => m.ProviderQualification, config => config.Ignore())
                 .ForMember(m => m.QualificationRoutePathMapping, config => config.Ignore())
                 .ForMember(m => m.CreatedBy, config => config.MapFrom<LoggedInUserNameResolver<AddQualificationViewModel, Qualification>>())
@@ -35,6 +49,13 @@ namespace Sfa.Tl.Matching.Application.Mappers
             CreateMap<MissingQualificationViewModel, Qualification>()
                 .ForMember(m => m.Id, config => config.Ignore())
                 .ForMember(m => m.LarsId, config => config.MapFrom(s => s.LarId))
+
+                .ForMember(m => m.QualificationSearch, config => config.Ignore())
+                .ForMember(m => m.ShortTitleSearch, config => config.Ignore())
+
+                //.ForMember(m => m.QualificationSearch, config => config.MapFrom(s => GetSearchTerm(s.Title, s.ShortTitle)))
+                //.ForMember(m => m.ShortTitleSearch, config => config.MapFrom(s => GetSearchTerm(s.ShortTitle)))
+                
                 .ForMember(m => m.ProviderQualification, config => config.Ignore())
                 .ForMember(m => m.QualificationRoutePathMapping, config => config.Ignore())
                 .ForMember(m => m.CreatedBy, config => config.MapFrom<LoggedInUserNameResolver<MissingQualificationViewModel, Qualification>>())
@@ -42,6 +63,15 @@ namespace Sfa.Tl.Matching.Application.Mappers
                 .ForMember(m => m.ModifiedOn, config => config.Ignore())
                 .ForMember(m => m.ModifiedBy, config => config.Ignore())
                 ;
+        }
+
+        private static string GetSearchTerm(params string[] searchTerms)
+        {
+            var searchTerm = new StringBuilder();
+            foreach (var term in searchTerms)
+                searchTerm.Append(term.ToQualificationSearch());
+
+            return searchTerm.ToString();
         }
     }
 }
