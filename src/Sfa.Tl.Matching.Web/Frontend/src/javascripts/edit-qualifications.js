a88c6f285699323e856cca1bb5d5902e9d7087e6
Ashley Anderton <ashley.j.anderton@gmail.com>
2019-06-07 15:51:12 +0100

    Fixed submit intercept, now alerts on submit

--- src/Sfa.Tl.Matching.Web/Frontend/src/javascripts/edit-qualifications.js
+++ src/Sfa.Tl.Matching.Web/Frontend/src/javascripts/edit-qualifications.js
@@ -1,3 +1,9 @@
-$('.tl-expandable--header').click(function () {
+$('.tl-expandable--header').click(function (e) {
+    e.preventDefault();
     $(this).parent().toggleClass('active');
+});
+
+$('.tl-editquals-item').submit(function (event) {
+    event.preventDefault();
+    alert('form submitted');
 });
