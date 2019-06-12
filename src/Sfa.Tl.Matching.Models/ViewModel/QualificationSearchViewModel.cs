42d35722dc8d9c0c87bcf57ecce410251f3f8c8e
Adam <adamesfa@outlook.com>
2019-06-12 15:52:24 +0100

    Fix issue when searching for less than characters

--- src/Sfa.Tl.Matching.Models/ViewModel/QualificationSearchViewModel.cs
+++ src/Sfa.Tl.Matching.Models/ViewModel/QualificationSearchViewModel.cs
@@ -7,7 +7,7 @@ namespace Sfa.Tl.Matching.Models.ViewModel
     {
         [Required(ErrorMessage = "You must enter 2 or more letters for your search")]
         [MinLength(2, ErrorMessage = "You must enter 2 or more letters for your search")]
-        [RegularExpression(@"^(?!^\d+$)^.+$", ErrorMessage = "You must enter 2 or more letters for your search")]
+        [RegularExpression(@"[a-zA-Z]+", ErrorMessage = "You must enter 2 or more letters for your search")]
         public string SearchTerms { get; set; }
         public IList<QualificationSearchResultViewModel> Results { get; set; } = new List<QualificationSearchResultViewModel>();
         public int ResultCount { get; set; }
