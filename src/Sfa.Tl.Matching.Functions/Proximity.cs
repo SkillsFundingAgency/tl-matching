800253031d16cc88c448b0d5b9f631dd6979406c
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-06-14 00:08:09 +0100

    Added BackFillPostTown Function

--- src/Sfa.Tl.Matching.Functions/Proximity.cs
+++ src/Sfa.Tl.Matching.Functions/Proximity.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
@@ -12,22 +14,52 @@ namespace Sfa.Tl.Matching.Functions
     public class Proximity
     {
         // ReSharper disable once UnusedMember.Global
+        [FunctionName("BackFillPostTown")]
+        public async Task BackFillPostTown(
+            [TimerTrigger("0 0 0 1 1 *", RunOnStartup = true)]
+            TimerInfo timer,
             ExecutionContext context,
             ILogger logger,
+            [Inject] IGoogleMapApiClient googleMapApiClient,
+            [Inject] IRepository<ProviderVenue> providerVenueRepository,
             [Inject] IRepository<FunctionLog> functionlogRepository
         )
         {
             try
             {
+                var stopwatch = Stopwatch.StartNew();
+
+                logger.LogInformation($"Function {context.FunctionName} triggered");
+
+                var providerVenues = new List<ProviderVenue>();
+
+                foreach (var providerVenue in providerVenueRepository.GetMany(pv => pv.Town == null || pv.Town == "" || pv.Town == " "))
+                {
+                    var googleAddressdetail = await googleMapApiClient.GetAddressDetails(providerVenue.Postcode);
+
+                    providerVenue.Town = googleAddressdetail;
+
+                    providerVenues.Add(providerVenue);
+                }
+
+                await providerVenueRepository.UpdateMany(providerVenues);
+
+                stopwatch.Stop();
+
+                logger.LogInformation($"Function {context.FunctionName} finished processing\n" +
+                                      $"\tRows saved: {providerVenues.Count}\n" +
+                                      $"\tTime taken: {stopwatch.ElapsedMilliseconds: #,###}ms");
             }
             catch (Exception e)
             {
+                var errormessage = $"Error importing BackFillPostTown Data. Internal Error Message {e}";
 
                 logger.LogError(errormessage);
 
                 await functionlogRepository.Create(new FunctionLog
                 {
                     ErrorMessage = errormessage,
+                    FunctionName = nameof(QualificationSearchColumns),
                     RowNumber = -1
                 });
                 throw;
