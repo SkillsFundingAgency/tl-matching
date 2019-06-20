5772b294138698ee097f07aa117c2ea8fa93f8cd
Adam <adamesfa@outlook.com>
2019-06-20 08:09:47 +0100

    TLWP-764 - Add single referral partial view with view models

---
+++ src/Sfa.Tl.Matching.Models/ViewModel/OpportunityBasketReferralItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Sfa.Tl.Matching.Models.ViewModel
+{
+    public class OpportunityBasketReferralItemViewModel
+    {
+        public string Workplace { get; set; }
+        public string JobRole { get; set; }
+        public string StudentsWanted { get; set; }
+        public int Providers { get; set; }
+    }
+}
---
+++ src/Sfa.Tl.Matching.Models/ViewModel/OpportunityBasketReferralViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Sfa.Tl.Matching.Models.ViewModel
+{
+    public class OpportunityBasketReferralViewModel
+    {
+        public IList<OpportunityBasketReferralItemViewModel> Items { get; set; }
+    }
+}
