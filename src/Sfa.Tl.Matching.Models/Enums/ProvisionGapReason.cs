cfb5a64a2ffeac3c287e6671407274e64f246c9c
Mike Wild <wildconsultingltd@googlemail.com>
2019-06-20 14:34:36 +0100

    Add enum for Provision Gap reaons

---
+++ src/Sfa.Tl.Matching.Models/Enums/ProvisionGapReason.cs
@@ -0,0 +1,9 @@
+﻿namespace Sfa.Tl.Matching.Models.Enums
+{
+    public enum ProvisionGapReason
+    {
+        NoSuitableStudent = 1,
+        HadBadExperience,
+        ProvidersTooFarAway
+    }
+}
