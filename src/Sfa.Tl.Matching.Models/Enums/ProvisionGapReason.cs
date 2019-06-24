7ab8dc31b87697de09a471f24e4cb44a0d701d76
Mike Wild <wildconsultingltd@googlemail.com>
2019-06-24 11:35:20 +0100

    Remove unused ProvisionGapReason enum

--- src/Sfa.Tl.Matching.Models/Enums/ProvisionGapReason.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sfa.Tl.Matching.Models.Enums
-{
-    public enum ProvisionGapReason
-    {
-        NoSuitableStudent = 1,
-        HadBadExperience,
-        ProvidersTooFarAway
-    }
-}
