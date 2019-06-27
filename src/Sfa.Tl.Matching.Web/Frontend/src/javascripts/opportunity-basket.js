e921e6007642e071d74b7873b7ab56d9344e6434
Ashley Anderton <ashley.j.anderton@gmail.com>
2019-06-27 11:02:36 +0100

    Fixed select all checkbox stuff. (Run gulp to get it working)

--- src/Sfa.Tl.Matching.Web/Frontend/src/javascripts/opportunity-basket.js
+++ src/Sfa.Tl.Matching.Web/Frontend/src/javascripts/opportunity-basket.js
@@ -3,3 +3,14 @@
 $('.tl-selectall').click(function () {
     $('.tl-checkbox').prop('checked', this.checked);
 });
+
+
+$(".tl-checkbox").change(function () {
+    if ($('.tl-checkbox:checked').length == $('.tl-checkbox').length) {
+        $('.tl-selectall').prop('checked', true);
+    }
+
+    else {
+        $('.tl-selectall').prop('checked', false);
+    }
+});
