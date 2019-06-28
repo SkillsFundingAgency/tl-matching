fb3c42aeed0abb0ef35ed3aa677602f941d9a220
Adam <adamesfa@outlook.com>
2019-06-28 13:25:42 +0100

    Set employer name default value

--- src/Sfa.Tl.Matching.Web/Frontend/src/javascripts/employer-search.js
+++ src/Sfa.Tl.Matching.Web/Frontend/src/javascripts/employer-search.js
@@ -5,7 +5,7 @@ var employer = (function () {
     var timeoutId;
 
     accessibleAutocomplete.enhanceSelectElement({
-        defaultValue: "",
+        defaultValue: $("#companyNameHidden").val(),
         autoSelect: true,
         selectElement: document.querySelector("#CompanyName"),
         minLength: queryMinLength,
@@ -15,7 +15,6 @@ var employer = (function () {
     });
 
     $("#SelectedEmployerId").val("");
-    $("#CompanyName").val($("#companyNameHidden").val());
 
     function search(query, populateResults) {
         if (query.trim().length < queryMinLength) return;
