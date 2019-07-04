be68e1f292b0a4a01984110507f0891a8572db3d
Adam <adamesfa@outlook.com>
2019-07-04 13:48:10 +0100

    Add Contact details to screen

--- src/Sfa.Tl.Matching.Models/ViewModel/EmployerConsentViewModel.cs
+++ src/Sfa.Tl.Matching.Models/ViewModel/EmployerConsentViewModel.cs
@@ -8,6 +8,9 @@ namespace Sfa.Tl.Matching.Models.ViewModel
         public int OpportunityItemId { get; set; }
         public int OpportunityItemCount { get; set; }
         public string EmployerName { get; set; }
+        public string EmployerContact { get; set; }
+        public string EmployerContactEmail { get; set; }
+        public string EmployerContactPhone { get; set; }
         public string CompanyName { get; set; }
         [Range(typeof(bool), "true", "true", ErrorMessage = "You must confirm that we can share the employer’s details with the selected providers")]
         public bool ConfirmationSelected { get; set; }
