48170077fecce3e0681039b28ffd4188e510b432
Mike Wild <wildconsultingltd@googlemail.com>
2019-07-04 09:25:08 +0100

    Reroute directly to GetSavedEmployer from basket

--- src/Sfa.Tl.Matching.Web/Controllers/NavigationController.cs
+++ src/Sfa.Tl.Matching.Web/Controllers/NavigationController.cs
@@ -62,17 +62,17 @@ namespace Sfa.Tl.Matching.Web.Controllers
         [Route("clear-opportunity-basket-is-selected-for-referral/{opportunityId}-{opportunityItemId}", Name = "ClearReferralsAndGoToOpportunityBasket")]
         public async Task<IActionResult> ClearReferralsAndGoToOpportunityBasket(int opportunityId, int opportunityItemId)
         {
-            //TODO: Do we need an extra flag to say reset?
             await ResetIsSelectedForReferral(opportunityId);
+
             return RedirectToRoute("GetOpportunityBasket", new { opportunityId, opportunityItemId });
         }
 
         [HttpGet]
-        [Route("save-employer-opportunity/{opportunityId}-{opportunityItemId}", Name = "SaveEmployerOpportunity")]
+        [Route("save-employer-opportunity/{opportunityId}", Name = "SaveEmployerOpportunity")]
         public async Task<IActionResult> SaveEmployerOpportunity(int opportunityId)
         {
-            //TODO: Do we need an extra flag to say reset?
             await ResetIsSelectedForReferral(opportunityId);
+
             return RedirectToRoute("GetSavedEmployerOpportunity");
         }
 
