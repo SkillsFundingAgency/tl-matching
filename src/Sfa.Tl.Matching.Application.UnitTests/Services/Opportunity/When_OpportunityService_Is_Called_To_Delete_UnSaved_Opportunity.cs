955205976df6f8fc55edc88fb62273a8386e3463
Nandakumar Kolandaivelu <nandakumar.velu@gmail.com>
2019-07-04 16:20:23 +0100

    Unit test for Delete Opportunity items, Referrals and Provision Gaps

--- src/Sfa.Tl.Matching.Application.UnitTests/Services/Opportunity/When_OpportunityService_Is_Called_To_Delete_UnSaved_Opportunity.cs
+++ src/Sfa.Tl.Matching.Application.UnitTests/Services/Opportunity/When_OpportunityService_Is_Called_To_Delete_UnSaved_Opportunity.cs
@@ -19,6 +19,8 @@ namespace Sfa.Tl.Matching.Application.UnitTests.Services.Opportunity
 
         private readonly IOpportunityRepository _opportunityRepository;
         private readonly IRepository<OpportunityItem> _opportunityItemRepository;
+        private readonly IRepository<Domain.Models.Referral> _referralRepository;
+        private readonly IRepository<ProvisionGap> _provisionGapRepository;
 
         public When_OpportunityService_Is_Called_To_Delete_UnSaved_Opportunity()
         {
@@ -26,26 +28,22 @@ namespace Sfa.Tl.Matching.Application.UnitTests.Services.Opportunity
 
             _opportunityRepository = Substitute.For<IOpportunityRepository>();
             _opportunityItemRepository = Substitute.For<IRepository<OpportunityItem>>();
-            var provisionGapRepository = Substitute.For<IRepository<ProvisionGap>>();
-            var referralRepository = Substitute.For<IRepository<Domain.Models.Referral>>();
+            _provisionGapRepository = Substitute.For<IRepository<ProvisionGap>>();
+            _referralRepository = Substitute.For<IRepository<Domain.Models.Referral>>();
 
-           _opportunityItemRepository
+            _opportunityItemRepository
                 .GetMany(Arg.Any<Expression<Func<OpportunityItem, bool>>>())
-                .Returns(new List<OpportunityItem>
-                {
-                    new OpportunityItem
-                    {
-                        Id = 1,
-                        OpportunityId = OpportunityId,
-                        OpportunityType = OpportunityType.Referral.ToString(),
-                        IsSelectedForReferral = false,
-                        IsCompleted = false,
-                        CreatedBy = "CreatedBy",
-                        IsSaved = false
-                    }
-                }.AsQueryable());
+                .Returns(SetOpportunityItem().AsQueryable());
 
-            var opportunityService = new OpportunityService(mapper, _opportunityRepository, _opportunityItemRepository, provisionGapRepository, referralRepository);
+            _referralRepository
+                .GetMany(Arg.Any<Expression<Func<Domain.Models.Referral, bool>>>())
+                .Returns(SetReferrals().AsQueryable());
+
+            _provisionGapRepository
+                .GetMany(Arg.Any<Expression<Func<ProvisionGap, bool>>>())
+                .Returns(SetProvisionGaps().AsQueryable());
+
+            var opportunityService = new OpportunityService(mapper, _opportunityRepository, _opportunityItemRepository, _provisionGapRepository, _referralRepository);
 
             opportunityService.DeleteOpportunityItemAsync(OpportunityId, OpportunityItemId).GetAwaiter().GetResult();
 
@@ -62,5 +60,61 @@ namespace Sfa.Tl.Matching.Application.UnitTests.Services.Opportunity
         {
             _opportunityItemRepository.Received(1).DeleteMany(Arg.Any<List<OpportunityItem>>());
         }
+
+        [Fact]
+        public void Then_Delete_Referral_Is_Called_Exactly_Once()
+        {
+            _referralRepository.Received(1).DeleteMany(Arg.Any<List<Domain.Models.Referral>>());
+        }
+
+        [Fact]
+        public void Then_Delete_Provision_Gap_Is_Called_Exactly_Once()
+        {
+            _provisionGapRepository.Received(1).DeleteMany(Arg.Any<List<ProvisionGap>>());
+        }
+
+        private static IEnumerable<OpportunityItem> SetOpportunityItem()
+        {
+            return new List<OpportunityItem>
+            {
+                new OpportunityItem
+                {
+                    Id = 1,
+                    OpportunityId = OpportunityId,
+                    OpportunityType = OpportunityType.Referral.ToString(),
+                    IsSelectedForReferral = false,
+                    IsCompleted = false,
+                    CreatedBy = "CreatedBy",
+                    IsSaved = false
+                }
+            };
+        }
+
+        private static IEnumerable<Domain.Models.Referral> SetReferrals()
+        {
+            return new List<Domain.Models.Referral>
+            {
+                new Domain.Models.Referral
+                {
+                    Id = 1,
+                    OpportunityItemId = 1,
+                    OpportunityItem = SetOpportunityItem().FirstOrDefault(item => item.OpportunityId == OpportunityId)
+                }
+            };
+        }
+
+        private static IEnumerable<ProvisionGap> SetProvisionGaps()
+        {
+            return new List<ProvisionGap>
+            {
+                new ProvisionGap
+                {
+                    OpportunityItem = SetOpportunityItem().FirstOrDefault(item => item.OpportunityId == OpportunityId),
+                    Id = 1,
+                    OpportunityItemId = OpportunityItemId
+                }
+            };
+        }
+
     }
 }
