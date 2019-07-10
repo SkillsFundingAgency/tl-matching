cb07aea331bdc50fe022bd33a647b4844edb08f6
nandakumar0311 <52156765+nandakumar0311@users.noreply.github.com>
2019-07-10 09:28:01 +0100

    Added functionality to remove employer from the employer basket (#151)

--- src/Sfa.Tl.Matching.Application/Interfaces/IOpportunityService.cs
+++ src/Sfa.Tl.Matching.Application/Interfaces/IOpportunityService.cs
@@ -36,5 +36,6 @@ namespace Sfa.Tl.Matching.Application.Interfaces
         Task ConfirmOpportunities(int opportunityId);
         Task<string> GetCompanyNameWithAkaAsync(int? opportunityId);
         Task<NavigationViewModel> LoadCancelLink(int opportunityId, int opportunityItemId);
+        Task DeleteEmployerOpportunityItemAsync(int opportunityId);
     }
 }
