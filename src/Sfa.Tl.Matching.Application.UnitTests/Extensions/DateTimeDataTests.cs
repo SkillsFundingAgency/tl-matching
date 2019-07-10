2971b7d52971d5d4795bfafabe9531a5271d7fcb
Adam <adamesfa@outlook.com>
2019-07-10 15:48:32 +0100

    Fix build

--- src/Sfa.Tl.Matching.Application.UnitTests/Extensions/DateTimeDataTests.cs
+++ src/Sfa.Tl.Matching.Application.UnitTests/Extensions/DateTimeDataTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Sfa.Tl.Matching.Application.Extensions;
 using Xunit;
@@ -8,15 +9,20 @@ namespace Sfa.Tl.Matching.Application.UnitTests.Extensions
     [Trait("DateTime", "Data Tests")]
     public class DateTimeExtensionsTests
     {
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[] { new DateTime(2000, 1, 1, 16, 50, 55), "on", "04:50pm on 01 January 2000" },
+                new object[] { new DateTime(2017, 4, 1, 22, 22, 3), "on", "11:22pm on 01 April 2017" },
+                new object[] { new DateTime(2018, 8, 18, 6, 22, 45), "on", "07:22am on 18 August 2018" },
+                new object[] { new DateTime(2019, 12, 31, 13, 02, 16), "on", "01:02pm on 31 December 2019" }
+            };
+
         [Theory(DisplayName = "GetTimeWithDate Data Tests")]
-        [InlineData("01/01/2000 16:50:16", "on", "04:50pm on 01 January 2000")]
-        [InlineData("01/04/2017 22:22:16", "on", "11:22pm on 01 April 2017")]
-        [InlineData("18/08/2018 06:22:16", "on", "07:22am on 18 August 2018")]
-        [InlineData("31/12/2019 13:02:16", "on", "01:02pm on 31 December 2019")]
-        public void GetTimeWithDateDataTests(string dateTime, string seperator, string result)
+        [MemberData(nameof(Data))]
+        public void GetTimeWithDateDataTests1(DateTime dateTime, string seperator, string result)
         {
-            var dt = DateTime.Parse(dateTime);
-            dt.GetTimeWithDate(seperator).Should().Be(result);
+            dateTime.GetTimeWithDate(seperator).Should().Be(result);
         }
     }
 }
