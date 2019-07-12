3c0a80e6c5c1105c5659d1af66c522ee9a615d93
Nandakumar Kolandaivelu <nandakumar.velu@gmail.com>
2019-07-12 09:31:45 +0100

    Added numeric sequence and customized opportunity object for unit test

--- src/Sfa.Tl.Matching.Application.UnitTests/AutoDomain/DomainCustomization.cs
+++ src/Sfa.Tl.Matching.Application.UnitTests/AutoDomain/DomainCustomization.cs
@@ -7,18 +7,21 @@ namespace Sfa.Tl.Matching.Application.UnitTests.AutoDomain
     {
         public void Customize(IFixture fixture)
         {
+            fixture.RepeatCount = 2;
+            fixture.Customizations.Add(new NumericSequenceGenerator());
+
             var opportunity = fixture.Create<Opportunity>();
-            var item = fixture.Create<OpportunityItem>();
+            const int itemId = 100;
 
             fixture.Customize<OpportunityItem>(composer => 
                                                         composer.With(oi => oi.OpportunityId, opportunity.Id)
                                                                 .With(oi => oi.Opportunity, opportunity)
-                                                                .With(oi => oi.Id, item.Id));
-            
-            fixture.Customize<Referral>(composer => composer.With(referral => referral.OpportunityItemId, item.Id));
-            fixture.Customize<ProvisionGap>(composer => composer.With(gap => gap.OpportunityItemId, item.Id));
-            
-            fixture.RepeatCount = 2;
+                                                                .With(oi => oi.Id, itemId));
+
+            fixture.Customize<Referral>(composer => composer.With(referral => referral.OpportunityItemId, itemId));
+            fixture.Customize<ProvisionGap>(composer => composer.With(gap => gap.OpportunityItemId, itemId));
+
+            fixture.Customize<Opportunity>(composer => composer.With(op => op.Id, opportunity.Id));
 
         }
     }
