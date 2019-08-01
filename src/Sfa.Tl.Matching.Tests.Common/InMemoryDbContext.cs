eb2dd62147b2685eefd46369ab5a2747b526b0ab
Mike Wild <wildconsultingltd@googlemail.com>
2019-08-01 15:10:15 +0100

    Add common test InMemoryDbContext class

---
+++ src/Sfa.Tl.Matching.Tests.Common/InMemoryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Sfa.Tl.Matching.Data;
+
+namespace Sfa.Tl.Matching.Tests.Common
+{
+    public static class InMemoryDbContext
+    {
+        public static MatchingDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<MatchingDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .EnableSensitiveDataLogging()
+                .Options;
+            var context = new MatchingDbContext(options);
+
+            return context;
+        }
+    }
+}
