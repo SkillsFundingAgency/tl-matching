c1d8089846cc9c05eb5a38c6b5de47e943282264
Mike Wild <wildconsultingltd@googlemail.com>
2019-08-01 15:29:14 +0100

    Add function to backfill provider venue name

---
+++ src/Sfa.Tl.Matching.Functions.UnitTests/Provider/When_Provider_BackFillProviderVenueName_Function_Fires.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Timers;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
+using Sfa.Tl.Matching.Data.Interfaces;
+using Sfa.Tl.Matching.Data.Repositories;
+using Sfa.Tl.Matching.Domain.Models;
+using Sfa.Tl.Matching.Tests.Common;
+using Xunit;
+
+namespace Sfa.Tl.Matching.Functions.UnitTests.Provider
+{
+    public class When_Provider_BackFillProviderVenueName_Function_Fires
+    {
+        private readonly IList<ProviderVenue> _results;
+
+        public When_Provider_BackFillProviderVenueName_Function_Fires()
+        {
+            var logger = Substitute.For<ILogger<GenericRepository<ProviderVenue>>>();
+
+            using (var dbContext = InMemoryDbContext.Create())
+            {
+                dbContext.AddRange(new List<ProviderVenue>
+                {
+                    new ProviderVenue
+                    {
+                        Postcode = "CV1 2WT",
+                        Name = null
+                    },
+                    new ProviderVenue
+                    {
+                        Postcode = "CV2 1AA",
+                        Name = "Venue Name"
+                    }
+                });
+
+                dbContext.SaveChanges();
+
+                IRepository<ProviderVenue> providerVenueRepository = new GenericRepository<ProviderVenue>(logger, dbContext);
+
+                var providerFunctions = new Functions.Provider();
+
+                providerFunctions.BackFillProviderVenueName(new TimerInfo(new ConstantSchedule(TimeSpan.Zero), null),
+                    new ExecutionContext(), new NullLogger<Functions.Provider>(), providerVenueRepository,
+                    Substitute.For<IRepository<FunctionLog>>()).GetAwaiter().GetResult();
+
+                _results = dbContext.ProviderVenue.ToList();
+            }
+        }
+
+        [Fact]
+        public void ProviderVenueRepository_Results_Should_Have_Expected_Values()
+        {
+            _results.Count.Should().Be(2);
+            _results[0].Name.Should().Be("CV1 2WT");
+            _results[1].Name.Should().Be("Venue Name");
+        }
+    }
+}
---
+++ src/Sfa.Tl.Matching.Functions/Provider.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Sfa.Tl.Matching.Application.Extensions;
+using Sfa.Tl.Matching.Data.Interfaces;
+using Sfa.Tl.Matching.Domain.Models;
+using Sfa.Tl.Matching.Functions.Extensions;
+
+namespace Sfa.Tl.Matching.Functions
+{
+    public class Provider
+    {
+        [FunctionName("BackFillProviderDisplayName")]
+        public async Task BackFillProviderDisplayName(
+            [TimerTrigger("0 0 0 1 1 *", RunOnStartup = true)]
+            TimerInfo timer,
+            ExecutionContext context,
+            ILogger logger,
+            [Inject] IRepository<Domain.Models.Provider> providerRepository,
+            [Inject] IRepository<FunctionLog> functionlogRepository
+        )
+        {
+            //TODO: Implement this
+
+        }
+
+        [FunctionName("BackFillProviderVenueName")]
+        public async Task BackFillProviderVenueName(
+            [TimerTrigger("0 0 0 1 1 *", RunOnStartup = true)]
+            TimerInfo timer,
+            ExecutionContext context,
+            ILogger logger,
+            [Inject] IRepository<ProviderVenue> providerVenueRepository,
+            [Inject] IRepository<FunctionLog> functionlogRepository
+        )
+        {
+            try
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                logger.LogInformation($"Function {context.FunctionName} triggered");
+
+                var providerVenues = new List<ProviderVenue>();
+
+                foreach (var providerVenue in providerVenueRepository.GetMany(pv => string.IsNullOrWhiteSpace(pv.Name)))
+                {
+                    providerVenue.Name = providerVenue.Postcode;
+
+                    providerVenues.Add(providerVenue);
+                }
+
+                await providerVenueRepository.UpdateMany(providerVenues);
+
+                stopwatch.Stop();
+
+                logger.LogInformation($"Function {context.FunctionName} finished processing\n" +
+                                      $"\tRows saved: {providerVenues.Count}\n" +
+                                      $"\tTime taken: {stopwatch.ElapsedMilliseconds: #,###}ms");
+            }
+            catch (Exception e)
+            {
+                var errormessage = $"Error Back Filling Provider Venue Name data. Internal Error Message {e}";
+
+                logger.LogError(errormessage);
+
+                await functionlogRepository.Create(new FunctionLog
+                {
+                    ErrorMessage = errormessage,
+                    FunctionName = nameof(BackFillProviderVenueName),
+                    RowNumber = -1
+                });
+                throw;
+            }
+        }
+    }
+}
