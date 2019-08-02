bf9ba9071b4c4251ab34c69380e0197ac0ec7e34
Mike Wild <wildconsultingltd@googlemail.com>
2019-08-02 10:39:16 +0100

    Implement Provider Display Name backfill

--- src/Sfa.Tl.Matching.Functions/Provider.cs
+++ src/Sfa.Tl.Matching.Functions/Provider.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Sfa.Tl.Matching.Application.Extensions;
 using Sfa.Tl.Matching.Data.Interfaces;
 using Sfa.Tl.Matching.Domain.Models;
 using Sfa.Tl.Matching.Functions.Extensions;
@@ -22,8 +24,54 @@ namespace Sfa.Tl.Matching.Functions
             [Inject] IRepository<FunctionLog> functionlogRepository
         )
         {
-            //TODO: Implement this
+            try
+            {
+                var stopwatch = Stopwatch.StartNew();
 
+                logger.LogInformation($"Function {context.FunctionName} triggered");
+
+                var providers = new List<Domain.Models.Provider>();
+
+                if (providerRepository.GetMany(p => string.IsNullOrWhiteSpace(p.DisplayName)).Any())
+                {
+                    foreach (var provider in providerRepository.GetMany(p => true))
+                    {
+                        var displayName =
+                            string.IsNullOrWhiteSpace(provider.DisplayName)
+                                ? provider.Name
+                                : provider.DisplayName;
+
+                        displayName = displayName.ToTitleCase();
+                        if (displayName != provider.DisplayName)
+                        {
+                            provider.DisplayName = displayName;
+                            providers.Add(provider);
+                        }
+                    }
+
+                    await providerRepository.UpdateMany(providers);
+                }
+
+                stopwatch.Stop();
+
+                logger.LogInformation($"Function {context.FunctionName} finished processing\n" +
+                                      $"\tRows saved: {providers.Count}\n" +
+                                      $"\tTime taken: {stopwatch.ElapsedMilliseconds: #,###}ms");
+            }
+            catch (Exception e)
+            {
+                var errormessage = $"Error Back Filling Provider Post Town Data. Internal Error Message {e}";
+
+                logger.LogError(errormessage);
+
+                await functionlogRepository.Create(new FunctionLog
+                {
+                    ErrorMessage = errormessage,
+                    FunctionName = nameof(BackFillProviderDisplayName),
+                    RowNumber = -1
+                });
+                throw;
+            }
         }
 
         [FunctionName("BackFillProviderVenueName")]
