09b8b0accf206d11ee894809a0293a75f2d6bff2
Adam <adamesfa@outlook.com>
2019-08-05 10:42:12 +0100

    Add end to end tests

---
+++ src/Sfa.Tl.Matching.EndToEndTests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace Sfa.Tl.Matching.EndToEndTests
+{
+    public class Class1
+    {
+        [Fact(DisplayName = "Create Single Referral")]
+        public void CreateReferral()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions))
+            {
+                driver.Navigate().GoToUrl("https://at.industryplacementmatching.education.gov.uk");
+
+                driver.Title.Should().Be("How to sign in - Match employers with providers for industry placements - GOV.UK");
+            }
+        }
+    }
+}
