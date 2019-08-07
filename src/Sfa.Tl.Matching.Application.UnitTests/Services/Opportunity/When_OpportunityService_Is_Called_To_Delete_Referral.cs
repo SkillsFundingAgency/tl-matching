c581233087076f6067cc3053107cec89dba07e48
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2019-08-07 16:01:53 +0100

    TLWP-828 - Added unit test coverage for removing referral

---
+++ src/Sfa.Tl.Matching.Application.UnitTests/Services/Opportunity/When_OpportunityService_Is_Called_To_Delete_Referral.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq.Expressions;
+using AutoMapper;
+using NSubstitute;
+using Sfa.Tl.Matching.Api.Clients.GoogleMaps;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Application.Mappers;
+using Sfa.Tl.Matching.Application.Services;
+using Sfa.Tl.Matching.Data.Interfaces;
+using Sfa.Tl.Matching.Domain.Models;
+using Sfa.Tl.Matching.Models.Dto;
+using Xunit;
+
+namespace Sfa.Tl.Matching.Application.UnitTests.Services.Opportunity
+{
+    public class When_OpportunityService_Is_Called_To_Delete_Referral
+    {
+        private const int ReferralId = 2;
+        private const int OpportunityItemId = 1;
+
+        private readonly IOpportunityRepository _opportunityRepository;
+        private readonly IRepository<OpportunityItem> _opportunityItemRepository;
+        private readonly IRepository<Domain.Models.Referral> _referralRepository;
+        private readonly IRepository<ProvisionGap> _provisionGapRepository;
+
+        public When_OpportunityService_Is_Called_To_Delete_Referral()
+        {
+            var config = new MapperConfiguration(c => c.AddMaps(typeof(OpportunityMapper).Assembly));
+            var mapper = new Mapper(config);
+
+            _opportunityRepository = Substitute.For<IOpportunityRepository>();
+            _opportunityItemRepository = Substitute.For<IRepository<OpportunityItem>>();
+            _provisionGapRepository = Substitute.For<IRepository<ProvisionGap>>();
+            _referralRepository = Substitute.For<IRepository<Domain.Models.Referral>>();
+
+            _referralRepository.GetFirstOrDefault(Arg.Any<Expression<Func<Domain.Models.Referral, bool>>>())
+                .Returns(new Domain.Models.Referral
+                {
+                    Id = ReferralId,
+                    OpportunityItemId = OpportunityItemId
+                });
+
+            var googleMapApiClient = Substitute.For<IGoogleMapApiClient>();
+            var opportunityPipelineReportWriter = Substitute.For<IFileWriter<OpportunityReportDto>>();
+            var dateTimeProvider = Substitute.For<IDateTimeProvider>();
+
+            var opportunityService = new OpportunityService(mapper, _opportunityRepository, _opportunityItemRepository,
+                _provisionGapRepository, _referralRepository, googleMapApiClient,
+                opportunityPipelineReportWriter, dateTimeProvider);
+
+            opportunityService.DeleteReferralAsync(ReferralId).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public void Then_Delete_Referral_Is_Called_Exactly_Once()
+        {
+            _referralRepository.Received(1).Delete(Arg.Any<Domain.Models.Referral>());
+        }
+
+        [Fact]
+        public void Then_Delete_Referral_Is_Called_With_Expected_Values()
+        {
+            _referralRepository
+                .Received()
+                .Delete(Arg.Is<Domain.Models.Referral>(
+                    r => r.Id == ReferralId));
+        }
+    }
+}
---
+++ src/Sfa.Tl.Matching.Web.UnitTests/Controllers/Opportunity/When_Opportunity_Controller_Delete_Referral.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Web.Controllers;
+using Sfa.Tl.Matching.Web.Mappers;
+using Sfa.Tl.Matching.Web.UnitTests.Controllers.Builders;
+using Xunit;
+
+namespace Sfa.Tl.Matching.Web.UnitTests.Controllers.Opportunity
+{
+    public class When_Opportunity_Controller_Delete_Referral
+    {
+        private readonly IOpportunityService _opportunityService;
+        private readonly IActionResult _result;
+        private const int OpportunityItemId = 2;
+        private const int ReferralIdToDelete = 3;
+
+        public When_Opportunity_Controller_Delete_Referral()
+        {
+            var config = new MapperConfiguration(c => c.AddMaps(typeof(CheckAnswersDtoMapper).Assembly));
+            var mapper = new Mapper(config);
+
+            _opportunityService = Substitute.For<IOpportunityService>();
+
+            var opportunityController = new OpportunityController(_opportunityService, mapper);
+            var controllerWithClaims = new ClaimsBuilder<OpportunityController>(opportunityController)
+                .AddUserName("CreatedBy")
+                .Build();
+
+            _result = controllerWithClaims.DeleteReferral(ReferralIdToDelete, OpportunityItemId).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public void Then_Delete_Referral_Result_Is_Correct()
+        {
+            _result.Should().BeAssignableTo<RedirectToRouteResult>();
+            _result.Should().NotBeNull();
+
+            var result = _result as RedirectToRouteResult;
+
+            result?.RouteName.Should().Be("GetCheckAnswers");
+            result?.RouteValues["opportunityItemId"].Should().Be(OpportunityItemId);
+        }
+
+        [Fact]
+        public void Then_DeleteReferralAsync_Is_Called_Exactly_Once_In_Correct_Order()
+        {
+            _opportunityService.Received(1).DeleteReferralAsync(ReferralIdToDelete);
+        }
+    }
+}
