380d0794b28d055c92656478bc8f023e6d9769a4
Mike Wild <wildconsultingltd@googlemail.com>
2019-08-13 14:26:02 +0100

    Do not delete OLDOpportunity table in deployment

--- src/Sfa.Tl.Matching.Database/PostDeployment/Remove Obsolete Tables.sql
+++ src/Sfa.Tl.Matching.Database/PostDeployment/Remove Obsolete Tables.sql
@@ -1,2 +1,2 @@
-﻿IF EXISTS (SELECT 1 FROM sys.tables WHERE [Name] = 'OLDOpportunity' and schema_id =  SCHEMA_ID('dbo'))
-	DROP TABLE [dbo].[OLDOpportunity]
+﻿--IF EXISTS (SELECT 1 FROM sys.tables WHERE [Name] = 'OLDOpportunity' and schema_id =  SCHEMA_ID('dbo'))
+--	DROP TABLE [dbo].[OLDOpportunity]
