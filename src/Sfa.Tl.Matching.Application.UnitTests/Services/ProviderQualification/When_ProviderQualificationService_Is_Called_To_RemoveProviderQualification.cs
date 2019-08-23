230dbed75d934f3ba9db9188291489dda07cde2d
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-08-23 15:40:40 +0100

    fix unit tests for Deleting duplicate provider Qualification

--- src/Sfa.Tl.Matching.Application.UnitTests/Services/ProviderQualification/When_ProviderQualificationService_Is_Called_To_RemoveProviderQualification.cs
+++ src/Sfa.Tl.Matching.Application.UnitTests/Services/ProviderQualification/When_ProviderQualificationService_Is_Called_To_RemoveProviderQualification.cs
@@ -1,49 +1,70 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
+using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
 using NSubstitute;
 using Sfa.Tl.Matching.Application.Mappers;
 using Sfa.Tl.Matching.Application.Services;
+using Sfa.Tl.Matching.Data;
 using Sfa.Tl.Matching.Data.Interfaces;
+using Sfa.Tl.Matching.Data.Repositories;
+using Sfa.Tl.Matching.Tests.Common.Extensions;
 using Xunit;
 
 namespace Sfa.Tl.Matching.Application.UnitTests.Services.ProviderQualification
 {
     public class When_ProviderQualificationService_Is_Called_To_RemoveProviderQualification
     {
-        private readonly IRepository<Domain.Models.ProviderQualification> _providerQualificationRepository;
+        private readonly MatchingDbContext _mockContext;
 
         public When_ProviderQualificationService_Is_Called_To_RemoveProviderQualification()
         {
             var config = new MapperConfiguration(c => c.AddMaps(typeof(QualificationMapper).Assembly));
             var mapper = new Mapper(config);
 
-            _providerQualificationRepository = Substitute.For<IRepository<Domain.Models.ProviderQualification>>();
-            _providerQualificationRepository.GetMany(Arg.Any<Expression<Func<Domain.Models.ProviderQualification, bool>>>())
-                .Returns(new List<Domain.Models.ProviderQualification> {new Domain.Models.ProviderQualification
-                {
-                    Id = 1,
-                    ProviderVenueId = 1,
-                    QualificationId = 2
-                }}.AsQueryable());
+            var mockSet = Substitute.For<DbSet<Domain.Models.ProviderQualification>, IAsyncEnumerable<Domain.Models.ProviderQualification>, IQueryable<Domain.Models.ProviderQualification>>();
 
-            var providerQualificationService = new ProviderQualificationService(mapper, _providerQualificationRepository);
+            var providerQualifications = new List<Domain.Models.ProviderQualification> {
+                new Domain.Models.ProviderQualification
+                    {
+                        Id = 1,
+                        ProviderVenueId = 1,
+                        QualificationId = 2
+                    }}.AsQueryable();
+
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            ((IAsyncEnumerable<Domain.Models.ProviderQualification>)mockSet).GetEnumerator()
+                .Returns(new FakeAsyncEnumerator<Domain.Models.ProviderQualification>(providerQualifications.GetEnumerator()));
+            ((IQueryable<Domain.Models.ProviderQualification>)mockSet).Provider.Returns(
+                new FakeAsyncQueryProvider<Domain.Models.ProviderQualification>(providerQualifications.Provider));
+            ((IQueryable<Domain.Models.ProviderQualification>)mockSet).Expression.Returns(providerQualifications.Expression);
+            ((IQueryable<Domain.Models.ProviderQualification>)mockSet).ElementType.Returns(providerQualifications.ElementType);
+            ((IQueryable<Domain.Models.ProviderQualification>)mockSet).GetEnumerator().Returns(providerQualifications.GetEnumerator());
+
+            var contextOptions = new DbContextOptions<MatchingDbContext>();
+            _mockContext = Substitute.For<MatchingDbContext>(contextOptions);
+            _mockContext.Set<Domain.Models.ProviderQualification>().Returns(mockSet);
+
+            IRepository<Domain.Models.ProviderQualification> repository = new GenericRepository<Domain.Models.ProviderQualification>(NullLogger<GenericRepository<Domain.Models.ProviderQualification>>.Instance, _mockContext);
+
+            var providerQualificationService = new ProviderQualificationService(mapper, repository);
 
             providerQualificationService.RemoveProviderQualificationAsync(1, 2).GetAwaiter().GetResult();
         }
 
         [Fact]
-        public void Then_ProviderQualificationRepository_Delete_Is_Called_Exactly_Once_With_Expected_Values()
+        public async Task Then_ProviderQualificationRepository_Delete_Is_Called_Exactly_Once_With_Expected_Values()
         {
-            _providerQualificationRepository
+            _mockContext
                 .Received(1)
-                .DeleteMany(Arg.Is<IList<Domain.Models.ProviderQualification>>(
+                .RemoveRange(Arg.Is<IList<Domain.Models.ProviderQualification>>(
                     p => p.First().ProviderVenueId == 1 &&
-                         p.First().QualificationId == 2
-                        ));
+                            p.First().QualificationId == 2
+                            ));
+
+           await _mockContext.Received(1).SaveChangesAsync();
         }
     }
 }
