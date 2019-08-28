f4f52873922efc513f860504b736b161fb411a0b
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-08-28 16:31:52 +0100

    Added DbQuery for ServiceOpportunityReportDto and ServiceProviderOpportunityReportDto,
    fixed errors in SQL files
    converted all non string types to nullable in ServiceOpportunityReportDto
    
    (cherry picked from commit 729edd62213731cad980132e00128f19b63a022d)
    
    # Conflicts:
    #	src/Sfa.Tl.Matching.Data/MatchingDbContext.cs

--- src/Sfa.Tl.Matching.Data/Repositories/ProviderOpportunityServiceReport.sql
+++ src/Sfa.Tl.Matching.Data/Repositories/ProviderOpportunityServiceReport.sql
@@ -1,14 +1,14 @@
 SELECT 
 	count(oi.Id) OpportunityItemCount, 
-	R.Name AS RouteName, 
+	R.[Name] AS RouteName, 
 	pv.postcode AS ProviderVenuePostCode, 
-	p.name AS ProviderName
-FROM OpportunityItem as OI
-INNER JOIN Referral as RF on OI.id = RF.OpportunityItemId
-INNER JOIN ProviderVenue AS PV on pv.Id = rf.ProviderVenueId
-INNER JOIN [Provider] AS P on pv.ProviderId = p.Id
-INNER JOIN [Route] as R on R.Id = oi.RouteId
-where 
+	p.[Name] AS ProviderName
+FROM OpportunityItem AS OI
+INNER JOIN Referral AS RF ON OI.id = RF.OpportunityItemId
+INNER JOIN ProviderVenue AS PV ON pv.Id = rf.ProviderVenueId
+INNER JOIN [Provider] AS P ON pv.ProviderId = p.Id
+INNER JOIN [Route] AS R ON R.Id = oi.RouteId
+WHERE 
 	oi.IsSaved = 1
 GROUP BY 
-	oi.RouteId, pv.postcode, p.name, p.id
+	R.[Name], pv.postcode, p.[Name], p.id
