5da98520401296cd763d0352a6629d498bf69f76
Adam <adamesfa@outlook.com>
2019-08-28 15:46:11 +0100

    Add missing mappings

--- src/Sfa.Tl.Matching.Application/Mappers/MaintenanceMapper.cs
+++ src/Sfa.Tl.Matching.Application/Mappers/MaintenanceMapper.cs
@@ -10,9 +10,12 @@ namespace Sfa.Tl.Matching.Application.Mappers
         public MaintenanceMapper()
         {
             CreateMap<MaintenanceViewModel, MaintenanceHistory>()
+                .ForMember(m => m.Id, config => config.Ignore())
                 .ForMember(m => m.IsOnline, config => config.MapFrom(s => s.IsOnline))
                 .ForMember(m => m.CreatedBy, config => config.MapFrom<LoggedInUserNameResolver<MaintenanceViewModel, MaintenanceHistory>>())
                 .ForMember(m => m.CreatedOn, config => config.Ignore())
+                .ForMember(m => m.ModifiedOn, config => config.Ignore())
+                .ForMember(m => m.ModifiedBy, config => config.Ignore())
                 ;
        }
     }
