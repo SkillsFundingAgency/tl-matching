c379cbbc6897485fe2fca10f650263595bc01c22
Adam <adamesfa@outlook.com>
2019-08-28 15:16:32 +0100

    Add Maintenance mapper

---
+++ src/Sfa.Tl.Matching.Application/Mappers/MaintenanceMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Sfa.Tl.Matching.Application.Mappers.Resolver;
+using Sfa.Tl.Matching.Domain.Models;
+using Sfa.Tl.Matching.Models.ViewModel;
+
+namespace Sfa.Tl.Matching.Application.Mappers
+{
+    public class MaintenanceMapper : Profile
+    {
+        public MaintenanceMapper()
+        {
+            CreateMap<MaintenanceViewModel, MaintenanceHistory>()
+                .ForMember(m => m.IsOnline, config => config.MapFrom(s => s.IsOnline))
+                .ForMember(m => m.CreatedBy, config => config.MapFrom<LoggedInUserNameResolver<MaintenanceViewModel, MaintenanceHistory>>())
+                .ForMember(m => m.CreatedOn, config => config.Ignore())
+                ;
+       }
+    }
+}
