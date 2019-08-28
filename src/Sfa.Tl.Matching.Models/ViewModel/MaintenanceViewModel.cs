31f7408f5c2fe84a2e8300ef22d076f7a2e45123
Adam <adamesfa@outlook.com>
2019-08-28 15:16:06 +0100

    Add Maintenance ViewModel

---
+++ src/Sfa.Tl.Matching.Models/ViewModel/MaintenanceViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Sfa.Tl.Matching.Models.ViewModel
+{
+    public class MaintenanceViewModel
+    {
+        public bool IsOnline { get; set; }
+
+        public string StatusDisplayText => IsOnline ? "Take offline" : "Bring online";
+        public string BodyDisplayText => IsOnline ? "online" : "offline";
+        public string ButtonClassName => IsOnline ? "tl-button--red govuk-button govuk-!-margin-right-1" : "govuk-button";
+    }
+}
