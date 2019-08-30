9fbb66b74bea95efc72790cedfbf5f6e75c0ae8c
Adam <adamesfa@outlook.com>
2019-08-30 16:25:57 +0100

    TLWP-677 - Maintenance page

--- src/Sfa.Tl.Matching.Application.UnitTests/Services/MaintenanceHistory/When_MaintenanceHistoryService_Is_Called_To_Get_Last.cs
+++ src/Sfa.Tl.Matching.Application.UnitTests/Services/MaintenanceHistory/When_MaintenanceHistoryService_Is_Called_To_Get_Last.cs
@@ -1,48 +1,52 @@
-﻿//using AutoMapper;
-//using FluentAssertions;
-//using NSubstitute;
-//using Sfa.Tl.Matching.Application.Mappers;
-//using Sfa.Tl.Matching.Application.Services;
-//using Sfa.Tl.Matching.Data.Interfaces;
-//using Sfa.Tl.Matching.Models.ViewModel;
-//using Xunit;
+﻿using System;
+using System.Linq.Expressions;
+using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.Matching.Application.Mappers;
+using Sfa.Tl.Matching.Application.Services;
+using Sfa.Tl.Matching.Data.Interfaces;
+using Sfa.Tl.Matching.Models.ViewModel;
+using Xunit;
 
-//namespace Sfa.Tl.Matching.Application.UnitTests.Services.MaintenanceHistory
-//{
-//    public class When_MaintenanceHistoryService_Is_Called_To_Get_Last
-//    {
-//        private readonly MaintenanceViewModel _result;
-//        private readonly IRepository<Domain.Models.MaintenanceHistory> _maintenanceHistoryRepository;
+namespace Sfa.Tl.Matching.Application.UnitTests.Services.MaintenanceHistory
+{
+    public class When_MaintenanceHistoryService_Is_Called_To_Get_Last
+    {
+        private readonly MaintenanceViewModel _result;
+        private readonly IRepository<Domain.Models.MaintenanceHistory> _maintenanceHistoryRepository;
 
-//        public When_MaintenanceHistoryService_Is_Called_To_Get_Last()
-//        {
-//            var config = new MapperConfiguration(c => c.AddMaps(typeof(MaintenanceMapper).Assembly));
-//            var mapper = new Mapper(config);
+        public When_MaintenanceHistoryService_Is_Called_To_Get_Last()
+        {
+            var config = new MapperConfiguration(c => c.AddMaps(typeof(MaintenanceMapper).Assembly));
+            var mapper = new Mapper(config);
 
-//            _maintenanceHistoryRepository = Substitute.For<IRepository<Domain.Models.MaintenanceHistory>>();
-//            _maintenanceHistoryRepository.GetLastOrDefault(mh => true).Returns(new Domain.Models.MaintenanceHistory
-//            {
-//                Id = 1,
-//                IsOnline = false
-//            });
-            
-//            var maintenanceHistoryService = new MaintenanceHistoryService(mapper, _maintenanceHistoryRepository);
-//            _result = maintenanceHistoryService.GetLatestMaintenanceHistory()
-//                .GetAwaiter().GetResult();
-//        }
+            _maintenanceHistoryRepository = Substitute.For<IRepository<Domain.Models.MaintenanceHistory>>();
+            _maintenanceHistoryRepository.GetLastOrDefault(
+                Arg.Any<Expression<Func<Domain.Models.MaintenanceHistory, bool>>>())
+                .Returns(new Domain.Models.MaintenanceHistory
+            {
+                Id = 1,
+                IsOnline = false
+            });
 
-//        [Fact]
-//        public void Then_GetLastOrDefault_Is_Called_Exactly_Once()
-//        {
-//            _maintenanceHistoryRepository
-//                .Received(1)
-//                .GetLastOrDefault(mh => true);
-//        }
+            var maintenanceHistoryService = new MaintenanceHistoryService(mapper, _maintenanceHistoryRepository);
+            _result = maintenanceHistoryService.GetLatestMaintenanceHistory()
+                .GetAwaiter().GetResult();
+        }
 
-//        [Fact]
-//        public void Then_ViewModel_Is_Correct()
-//        {
-//            _result.IsOnline.Should().BeFalse();
-//        }
-//    }
-//}
+        [Fact]
+        public void Then_GetLastOrDefault_Is_Called_Exactly_Once()
+        {
+            _maintenanceHistoryRepository
+                .Received(1)
+                .GetLastOrDefault(Arg.Any<Expression<Func<Domain.Models.MaintenanceHistory, bool>>>());
+        }
+
+        [Fact]
+        public void Then_ViewModel_Is_Correct()
+        {
+            _result.IsOnline.Should().BeFalse();
+        }
+    }
+}
