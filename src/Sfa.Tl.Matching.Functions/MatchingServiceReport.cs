35bd3e7c2d4004832239948ac2ebf846f164e3e0
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-09-04 13:32:52 +0100

    Added Employer Count Function

--- src/Sfa.Tl.Matching.Functions/MatchingServiceReport.cs
+++ src/Sfa.Tl.Matching.Functions/MatchingServiceReport.cs
@@ -5,7 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Sfa.Tl.Matching.Application.Interfaces;
 using Sfa.Tl.Matching.Data.Interfaces;
 using Sfa.Tl.Matching.Domain.Models;
 using Sfa.Tl.Matching.Functions.Extensions;
@@ -75,6 +77,46 @@ namespace Sfa.Tl.Matching.Functions
 
                 logger.LogInformation($"Function {context.FunctionName} finished processing\n" +
                                       $"\tRows saved: {result.Count}\n" +
+                                      $"\tTime taken: {stopwatch.ElapsedMilliseconds: #,###}ms");
+
+                return new JsonResult(result);
+            }
+            catch (Exception e)
+            {
+                var errormessage = $"Error Executing {context.FunctionName}. Internal Error Message {e}";
+
+                logger.LogError(errormessage);
+
+                await functionlogRepository.Create(new FunctionLog
+                {
+                    ErrorMessage = errormessage,
+                    FunctionName = "MatchingServiceReport",
+                    RowNumber = -1
+                });
+                throw;
+            }        }
+
+        [FunctionName("GetMatchingServiceEmployerReport")]
+        public static async Task<IActionResult> GetMatchingServiceEmployerReport(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, 
+        ExecutionContext context,
+        ILogger logger, 
+        [Inject] IRepository<Domain.Models.Employer> employerRepository,
+        [Inject] IRepository<FunctionLog> functionlogRepository
+        )
+        {
+            try
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                logger.LogInformation($"Function {context.FunctionName} triggered");
+
+                var result = await employerRepository.GetMany().CountAsync();
+
+                stopwatch.Stop();
+
+                logger.LogInformation($"Function {context.FunctionName} finished processing\n" +
+                                      $"\tRows saved: {result}\n" +
                                       $"\tTime taken: {stopwatch.ElapsedMilliseconds: #,###}ms");
 
                 return new JsonResult(result);
