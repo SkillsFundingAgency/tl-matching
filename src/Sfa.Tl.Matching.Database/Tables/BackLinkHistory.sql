bf7168201288193c38a0b605be730e1038eb25e4
Nandakumar Kolandaivelu <nandakumar.velu@gmail.com>
2019-09-05 11:48:26 +0100

    Fixed Release error in database

--- src/Sfa.Tl.Matching.Database/Tables/BackLinkHistory.sql
+++ src/Sfa.Tl.Matching.Database/Tables/BackLinkHistory.sql
@@ -1,8 +1,7 @@
-﻿CREATE TABLE [dbo].[UserCache]
+﻿CREATE TABLE [dbo].[BackLinkHistory]
 (
 	[Id] INT NOT NULL IDENTITY(1,1), 
-	[Key] NVARCHAR(200) NOT NULL,
-    [Value] NVARCHAR(MAX) NOT NULL, 
+	[CurrentUrl] NVARCHAR(MAX) NOT NULL, 
     [CreatedOn] DATETIME2 NOT NULL DEFAULT getutcdate(), 
     [CreatedBy] NVARCHAR(50) NULL, 
     [ModifiedOn] DATETIME2 NULL, 
