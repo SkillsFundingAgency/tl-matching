b5c072f039743ba7b5e68239b58fb92f71f28715
Nandakumar Kolandaivelu <nandakumar.velu@gmail.com>
2019-09-12 13:32:14 +0100

    Fixed a bug where the connection is closed intermittently

--- src/Sfa.Tl.Matching.Web/Filters/BackLinkFilter.cs
+++ src/Sfa.Tl.Matching.Web/Filters/BackLinkFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
 using Sfa.Tl.Matching.Application.Extensions;
@@ -6,7 +7,7 @@ using Sfa.Tl.Matching.Application.Interfaces;
 
 namespace Sfa.Tl.Matching.Web.Filters
 {
-    public class BackLinkFilter : IActionFilter
+    public class BackLinkFilter : IAsyncActionFilter
     {
         private readonly ILogger<BackLinkFilter> _logger;
         private readonly INavigationService _navigationService;
@@ -18,27 +19,25 @@ namespace Sfa.Tl.Matching.Web.Filters
             _navigationService = navigationService;
         }
 
-        public void OnActionExecuting(ActionExecutingContext context)
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             try
             {
-                if (context.HttpContext.Request.Method != "GET") return;
+                if (context.HttpContext.Request.Method == "GET")
+                {
+                    var path = context.HttpContext.Request.Path.ToString();
+                    var username = context.HttpContext.User.GetUserName();
 
-                var path = context.HttpContext.Request.Path.ToString();
-                var username = context.HttpContext.User.GetUserName();
+                    await _navigationService.AddCurrentUrl(path, username);
+                }
 
-                _navigationService.AddCurrentUrl(path, username);
+                await next();
 
             }
             catch (Exception exception)
             {
                 _logger.LogError(exception, exception.Message);
             }
-        }
-
-        public void OnActionExecuted(ActionExecutedContext context)
-        {
-
         }
     }
 
