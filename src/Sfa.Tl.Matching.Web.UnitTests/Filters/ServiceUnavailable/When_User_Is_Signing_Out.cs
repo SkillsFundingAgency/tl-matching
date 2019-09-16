f1ed835fc032bf04cd2f759514d49785dff73c2f
Adam <adamesfa@outlook.com>
2019-09-16 10:47:14 +0100

    Add unit tests back in

--- src/Sfa.Tl.Matching.Web.UnitTests/Filters/ServiceUnavailable/When_User_Is_Signing_Out.cs
+++ src/Sfa.Tl.Matching.Web.UnitTests/Filters/ServiceUnavailable/When_User_Is_Signing_Out.cs
@@ -1,69 +1,66 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Security.Claims;
-//using FluentAssertions;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.Controllers;
-//using Microsoft.AspNetCore.Mvc.Filters;
-//using Microsoft.AspNetCore.Routing;
-//using NSubstitute;
-//using Sfa.Tl.Matching.Application.Extensions;
-//using Sfa.Tl.Matching.Application.Interfaces;
-//using Sfa.Tl.Matching.Web.Controllers;
-//using Sfa.Tl.Matching.Web.Filters;
-//using Xunit;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using NSubstitute;
+using Sfa.Tl.Matching.Application.Extensions;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Web.Controllers;
+using Sfa.Tl.Matching.Web.Filters;
+using Xunit;
 
-//namespace Sfa.Tl.Matching.Web.UnitTests.Filters.ServiceUnavailable
-//{
-//    public class When_User_Is_Signing_Out
-//    {
-//        private readonly ActionExecutingContext _actionExecutingContext;
+namespace Sfa.Tl.Matching.Web.UnitTests.Filters.ServiceUnavailable
+{
+    public class When_User_Is_Signing_Out
+    {
+        private readonly ActionExecutingContext _actionExecutingContext;
 
-//        public When_User_Is_Signing_Out()
-//        {
-//            var claimsList = new List<Claim>
-//            {
-//                new Claim(ClaimTypes.Role, RolesExtensions.StandardUser)
-//            };
+        public When_User_Is_Signing_Out()
+        {
+            var claimsList = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, RolesExtensions.StandardUser)
+            };
 
-//            var user = new ClaimsPrincipal(new ClaimsIdentity(claimsList, "AuthenticationType"));
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claimsList, "AuthenticationType"));
 
-//            var defaultHttpContext = Substitute.For<HttpContext>();
-//            defaultHttpContext.User = user;
+            var defaultHttpContext = Substitute.For<HttpContext>();
+            defaultHttpContext.User = user;
 
-//            var routeData = new RouteData();
-//            routeData.Values.Add("controller", "Account");
-//            routeData.Values.Add("action", nameof(AccountController.SignOut));
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Account");
+            routeData.Values.Add("action", nameof(AccountController.SignOut));
 
-//            var controllerActionDescriptor = new ControllerActionDescriptor
-//            {
-//                ControllerName = "Account",
-//                ActionName = nameof(AccountController.SignOut)
-//            };
+            var controllerActionDescriptor = new ControllerActionDescriptor
+            {
+                ControllerName = "Account",
+                ActionName = nameof(AccountController.SignOut)
+            };
 
-//            var actionContext = new ActionContext(defaultHttpContext, routeData, controllerActionDescriptor);
+            var actionContext = new ActionContext(defaultHttpContext, routeData, controllerActionDescriptor);
 
-//            _actionExecutingContext = new ActionExecutingContext(
-//                actionContext,
-//                new List<IFilterMetadata>(),
-//                new Dictionary<string, object>(),
-//                null);
+            _actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>(),
+                null);
 
-//            var serviceStatusHistoryService = Substitute.For<IServiceStatusHistoryService>();
-//            var filterAttribute = new ServiceUnavailableFilterAttribute(serviceStatusHistoryService);
+            var serviceStatusHistoryService = Substitute.For<IServiceStatusHistoryService>();
+            var filterAttribute = new ServiceUnavailableFilterAttribute(serviceStatusHistoryService);
 
-//            var next = new ActionExecutionDelegate(() =>
-//            {
-//            });
+            var actionExecutionDelegate = Substitute.For<ActionExecutionDelegate>();
 
-//            filterAttribute.OnActionExecutionAsync(_actionExecutingContext, next).GetAwaiter().GetResult();
-//        }
+            filterAttribute.OnActionExecutionAsync(_actionExecutingContext, actionExecutionDelegate).GetAwaiter().GetResult();
+        }
 
-//        [Fact]
-//        public void Then_User_Does_Not_See_Service_Unavailable_Page()
-//        {
-//            _actionExecutingContext.Result.Should().BeNull();
-//        }
-//    }
-//}
+        [Fact]
+        public void Then_User_Does_Not_See_Service_Unavailable_Page()
+        {
+            _actionExecutingContext.Result.Should().BeNull();
+        }
+    }
+}
