ac8032a4f309e186a15fb12ca738b8728f41fdad
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-09-17 13:16:40 +0100

    Added Tests for Employer Created Event

---
+++ src/Sfa.Tl.Matching.Application.UnitTests/Services/Employer/Builders/CrmEmployerEventBaseBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Sfa.Tl.Matching.Models.Event;
+
+namespace Sfa.Tl.Matching.Application.UnitTests.Services.Employer.Builders
+{
+    public static class CrmEmployerEventBaseBuilder
+    {
+        public static CrmEmployerEventBase Buiild(bool isValid) => new CrmEmployerEventBase
+        {
+            ContactEmail = "Test@test.com",
+            ContactTelephone1 = "0123456789",
+            Name = "Test",
+            PrimaryContactId = null,
+            accountid = Guid.NewGuid().ToString(),
+            address1_line1 = "Test",
+            address1_postalcode = "Test",
+            customertypecode = new Customertypecode { Value = 200005 },
+            emailaddress1 = "Test",
+            owneremail = "Test",
+            owneridname = "test",
+            sfa_alias = "Test",
+            sfa_aupa = new SfaAupa { Value = isValid ? 229660000 : 0 },
+            sfa_ceasedtrading = 0,
+            sfa_employermanagement = null,
+            telephone1 = "Test"
+        };
+    }
+}
