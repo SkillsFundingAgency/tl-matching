35bce0a506c98a8ebf629d9c277ff548b2f0e26b
Nandakumar Kolandaivelu <nandakumar.velu@gmail.com>
2019-09-17 15:47:51 +0100

    Removed redundant properties from the usercache mapper

--- src/Sfa.Tl.Matching.Application/Mappers/UserCacheMapper.cs
+++ src/Sfa.Tl.Matching.Application/Mappers/UserCacheMapper.cs
@@ -10,14 +10,10 @@ namespace Sfa.Tl.Matching.Application.Mappers
         public UserCacheMapper()
         {
             CreateMap<UserCacheDto, UserCache>()
-                .ForMember(m => m.Id, config => config.MapFrom(s => s.Id))
-                .ForMember(m => m.Key, config => config.MapFrom(s => s.Key))
-                .ForMember(m => m.Value, config => config.MapFrom(s => s.Value))
                 .ForMember(m => m.CreatedBy, config => config.MapFrom<LoggedInUserNameResolver<UserCacheDto, UserCache>>())
                 .ForMember(m => m.CreatedOn, config => config.Ignore())
                 .ForMember(m => m.ModifiedBy, config => config.MapFrom<LoggedInUserNameResolver<UserCacheDto, UserCache>>())
-                .ForMember(m => m.ModifiedOn, config => config.MapFrom<UtcNowResolver<UserCacheDto, UserCache>>())
-                .ForAllOtherMembers(config => config.Ignore());
+                .ForMember(m => m.ModifiedOn, config => config.MapFrom<UtcNowResolver<UserCacheDto, UserCache>>());
         }
     }
 }
