dc13f6fb8ec8ffae65cbcae8df89c97da7e550cf
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-09-20 10:24:27 +0100

    Update CrmContactEventBase Defination

--- src/Sfa.Tl.Matching.Models/Event/CrmContactEventBase.cs
+++ src/Sfa.Tl.Matching.Models/Event/CrmContactEventBase.cs
@@ -2,21 +2,25 @@ namespace Sfa.Tl.Matching.Models.Event
 {
     public class CrmContactEventBase
     {
-        public string ContactEmail { get; set; }
-        public string ContactTelephone1 { get; set; }
-        public string Name { get; set; }
-        public PrimaryContactId PrimaryContactId { get; set; }
-        public string accountid { get; set; }
+        public string address1_city { get; set; }
+        public string address1_county { get; set; }
         public string address1_line1 { get; set; }
+        public string address1_line2 { get; set; }
+        public object address1_line3 { get; set; }
         public string address1_postalcode { get; set; }
+        public string contactid { get; set; }
         public Customertypecode customertypecode { get; set; }
-        public object emailaddress1 { get; set; }
-        public string owneremail { get; set; }
-        public string owneridname { get; set; }
-        public string sfa_alias { get; set; }
-        public SfaAupa sfa_aupa { get; set; }
-        public int sfa_ceasedtrading { get; set; }
-        public object sfa_employermanagement { get; set; }
+        public string emailaddress1 { get; set; }
+        public string fullname { get; set; }
+        public object mobilephone { get; set; }
+        public Parentcustomerid parentcustomerid { get; set; }
         public string telephone1 { get; set; }
+    }
+
+    public class Parentcustomerid
+    {
+        public string id { get; set; }
+        public string logicalname { get; set; }
+        public string name { get; set; }
     }
 }
