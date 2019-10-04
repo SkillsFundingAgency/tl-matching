59e047c2b654c27ef91e2bad9c121adfb9684e8e
Mike Wild <wildconsultingltd@googlemail.com>
2019-10-04 16:48:26 +0100

    Change distance search to use postcodes, not lat/long

--- src/Sfa.Tl.Matching.Api.Clients.UnitTests/Factories/GoogleDistanceMatrixHttpClientFactory.cs
+++ src/Sfa.Tl.Matching.Api.Clients.UnitTests/Factories/GoogleDistanceMatrixHttpClientFactory.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http;
 using Sfa.Tl.Matching.Api.Clients.GoogleDistanceMatrix;
 
@@ -5,12 +6,17 @@ namespace Sfa.Tl.Matching.Api.Clients.UnitTests.Factories
 {
     public class GoogleDistanceMatrixHttpClientFactory : HttpClientFactory
     {
-        public HttpClient Get(decimal originLatitude = 52.400997M, decimal originLongitude = -1.508122M, string destinationPolyline = "enc:chjyHb%7DW:", long arrivalTimeSeconds = 1570014314)
+        public HttpClient Get(string originPostcode = "CV1 2WT",
+            decimal originLatitude = 52.400997M, 
+            decimal originLongitude = -1.508122M, 
+            string destinationPostcode = "SW1A 2AA",
+            string destinationPolyline = "enc:chjyHb%7DW:", 
+            long arrivalTimeSeconds = 1570014314)
         {
             var response = new GoogleDistanceMatrixResponse
             {
                 DestinationAddresses = new[]
-                { 
+                {
                     "10 Downing St, Westminster, London SW1A 2AA, UK" 
                 },
                 OriginAddresses = new[]
@@ -43,8 +49,12 @@ namespace Sfa.Tl.Matching.Api.Clients.UnitTests.Factories
                 Status = "OK"
             };
 
+            var origin = WebUtility.UrlEncode(originPostcode);
+            var destination = WebUtility.UrlEncode(destinationPostcode);
+
             var uri =
-                $"https://example.com/distancematrix/json?units=imperial&origins={originLatitude}%2C{originLongitude}&mode=driving&arrival_time={arrivalTimeSeconds}&destinations={destinationPolyline}&key=TEST_KEY";
+                //$"https://example.com/distancematrix/json?units=imperial&origins={originLatitude}%2C{originLongitude}&mode=driving&arrival_time={arrivalTimeSeconds}&destinations={destinationPolyline}&key=TEST_KEY";
+                $"https://example.com/distancematrix/json?units=imperial&origins={origin}&mode=driving&arrival_time={arrivalTimeSeconds}&destinations={destination}&key=TEST_KEY";
 
             return CreateClient(response, uri);
         }
--- src/Sfa.Tl.Matching.Api.Clients/GoogleDistanceMatrix/IGoogleDistanceMatrixApiClient.cs
+++ src/Sfa.Tl.Matching.Api.Clients/GoogleDistanceMatrix/IGoogleDistanceMatrixApiClient.cs
@@ -6,6 +6,6 @@ namespace Sfa.Tl.Matching.Api.Clients.GoogleDistanceMatrix
 {
     public interface IGoogleDistanceMatrixApiClient
     {
-        Task<IDictionary<int, JourneyInfoDto>> GetJourneyTimesAsync(decimal latitude, decimal longitude, IList<LocationDto> destinations, string travelMode, long arrivalTimeSeconds);
+        Task<IDictionary<int, JourneyInfoDto>> GetJourneyTimesAsync(string originPostcode, decimal latitude, decimal longitude, IList<LocationDto> destinations, string travelMode, long arrivalTimeSeconds);
     }
 }
