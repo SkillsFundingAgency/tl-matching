67e9ee2805296489e43243c4efdc19208dbbd13d
Nandakumar Kolandaivelu <nandakumar.velu@gmail.com>
2019-10-07 09:23:13 +0100

    Replaced IRepository testing with InMemory dbcontext

---
+++ src/Sfa.Tl.Matching.Tests.Common/AutoDomain/MapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Application.Mappers.Resolver;
+
+namespace Sfa.Tl.Matching.Tests.Common.AutoDomain
+{
+    public static class MapperConfig<TMapper, TSource, TDest>
+    {
+        public static MapperConfiguration Config(HttpContextAccessor httpContextAccessor, IDateTimeProvider dateTimeProvider)
+        {
+            return new MapperConfiguration(c =>
+            {
+                c.AddMaps(typeof(TMapper).Assembly);
+                c.ConstructServicesUsing(type =>
+                    type.Name.Contains("LoggedInUserEmailResolver") ?
+                        new LoggedInUserEmailResolver<TSource, TDest>(httpContextAccessor) :
+                        type.Name.Contains("LoggedInUserNameResolver") ?
+                            (object)new LoggedInUserNameResolver<TSource, TDest>(httpContextAccessor) :
+                            type.Name.Contains("UtcNowResolver") ?
+                                new UtcNowResolver<TSource, TDest>(dateTimeProvider) :
+                                null);
+            });
+        }
+    }
+}
