d818402e868dd7f300ba65248f4527971c6e51db
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-10-07 15:50:22 +0100

    Added Ad-Hoc SQL to Update Invalid employerId in Opprtunity table

---
+++ Ad-Hoc SQL/Update Invalid employerId in Opprtunity Table.sql
@@ -0,0 +1,9 @@
+
+select * from Opportunity where id in (461, 462, 658, 714, 714)
+
+-- UPDATE Opportunity set EmployerId = 503007 WHERE id = 461
+-- UPDATE Opportunity set EmployerId = 505403 WHERE id = 462
+-- UPDATE Opportunity set EmployerId = 505382 WHERE id = 658
+-- UPDATE Opportunity set EmployerId = 505385 WHERE id = 714
+
+select * from Opportunity where id in (461, 462, 658, 714, 714)
