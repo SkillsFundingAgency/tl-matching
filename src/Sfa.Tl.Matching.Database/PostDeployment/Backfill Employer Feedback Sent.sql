44952f49b00abbd80a9f4fd0fdb059fd557001ba
Adam <adamesfa@outlook.com>
2019-10-09 14:05:00 +0100

    Remove old scripts
    (cherry picked from commit 776393982f66c8eba1456d5367a316c0fd7baf3b)

--- src/Sfa.Tl.Matching.Database/PostDeployment/Backfill Employer Feedback Sent.sql
+++ src/Sfa.Tl.Matching.Database/PostDeployment/Backfill Employer Feedback Sent.sql
@@ -1,17 +1,17 @@
-﻿SET NOCOUNT ON;
-SET XACT_ABORT ON;
-SET @scriptName = 'BackfillEmployerFeedbackSent';
-SET @TicketNo  = 'TLWP-840';
+﻿--SET NOCOUNT ON;
+--SET XACT_ABORT ON;
+--SET @scriptName = 'BackfillEmployerFeedbackSent';
+--SET @TicketNo  = 'TLWP-840';
 
-IF NOT EXISTS (SELECT 1 FROM [dbo].[DBProjDeployLog] WHERE [Name] = @scriptName )
-BEGIN
+--IF NOT EXISTS (SELECT 1 FROM [dbo].[DBProjDeployLog] WHERE [Name] = @scriptName )
+--BEGIN
 
-	UPDATE [OpportunityItem]
-	SET [EmployerFeedbackSent] = 1
-	WHERE [IsCompleted] = 1 
-	  AND CONVERT(date, [ModifiedOn]) < CONVERT(date, GETUTCDATE())
+--	UPDATE [OpportunityItem]
+--	SET [EmployerFeedbackSent] = 1
+--	WHERE [IsCompleted] = 1 
+--	  AND CONVERT(date, [ModifiedOn]) < CONVERT(date, GETUTCDATE())
 
-	INSERT INTO [dbo].[DBProjDeployLog]( [Date], [Name], [MD5], [Revision] )
-	VALUES( GETUTCDATE(), @scriptName, @TicketNo, 1 );
+--	INSERT INTO [dbo].[DBProjDeployLog]( [Date], [Name], [MD5], [Revision] )
+--	VALUES( GETUTCDATE(), @scriptName, @TicketNo, 1 );
 
-END;
+--END;
