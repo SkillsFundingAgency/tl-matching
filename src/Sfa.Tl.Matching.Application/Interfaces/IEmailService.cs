2e35ea724ec060c90e7af4de46b4d0dd20567577
Nandakumar Kolandaivelu <nandakumar.velu@gmail.com>
2019-10-11 14:14:53 +0100

    Created a function to receive notification response callback
    (cherry picked from commit 881fba4483ce487e27b1d8c98659990675567ce6)

--- src/Sfa.Tl.Matching.Application/Interfaces/IEmailService.cs
+++ src/Sfa.Tl.Matching.Application/Interfaces/IEmailService.cs
@@ -6,5 +6,6 @@ namespace Sfa.Tl.Matching.Application.Interfaces
     public interface IEmailService
     {
         Task SendEmailAsync(int? opportunityId, string templateName, string toAddress, IDictionary<string, string> personalisationTokens, string createdBy);
+        Task<int> HandleEmailStatusAsync(string payload);
     }
 }
