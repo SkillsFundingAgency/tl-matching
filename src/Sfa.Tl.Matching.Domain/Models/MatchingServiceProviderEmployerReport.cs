2396c7c5e18a177ff1863147197fdc964d9f9e0b
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-10-24 14:58:36 +0100

    Fix invalid data type for Placements and CreatedOn
    (cherry picked from commit 4fe58e0d3205daf2035d0e6572e83ad9f6318140)

--- src/Sfa.Tl.Matching.Domain/Models/MatchingServiceProviderEmployerReport.cs
+++ src/Sfa.Tl.Matching.Domain/Models/MatchingServiceProviderEmployerReport.cs
@@ -11,12 +11,12 @@ namespace Sfa.Tl.Matching.Domain.Models
         public string EmployerPostCode { get; set; }
         public string RouteName { get; set; }
         public string JobRole { get; set; }
-        public string Placements { get; set; }
         //public bool IsSaved { get; set; }
         //public bool IsCompleted { get; set; }
         //public string Owner { get; set; }
+        public int? Placements { get; set; }
         public string CreatedBy { get; set; }
-        public string CreatedOn { get; set; }
+        public DateTime? CreatedOn { get; set; }
         public DateTime? ModifiedOn { get; set; }
     }
 }
