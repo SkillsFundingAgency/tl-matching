6126db93b5c07b666ca7582e9bd4065f26d8d23c
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-12-03 10:48:11 +0000

    Added mappers for Route service

---
+++ src/Sfa.Tl.Matching.Application/Mappers/RouteMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Sfa.Tl.Matching.Domain.Models;
+using Sfa.Tl.Matching.Models.ViewModel;
+
+namespace Sfa.Tl.Matching.Application.Mappers
+{
+    public class RouteMapper : Profile
+    {
+        public RouteMapper()
+        {
+            CreateMap<Route, SelectListItem>()
+                .ForMember(m => m.Text, o => o.MapFrom(s => s.Id.ToString()))
+                .ForMember(m => m.Value, o => o.MapFrom(s => s.Name))
+                ;
+
+            CreateMap<Route, RouteSummaryViewModel>()
+                .ForMember(dest => dest.IsSelected, opt => opt.Ignore());
+        }
+    }
+}
