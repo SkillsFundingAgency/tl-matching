ad2e1e261857a6705f8880a97d415feeb4d8d274
Mike Wild <wildconsultingltd@googlemail.com>
2019-12-06 15:53:45 +0000

    Add initial ProviderFeedback function and timer settings

---
+++ src/Sfa.Tl.Matching.Application/Interfaces/IProviderFeedbackService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Sfa.Tl.Matching.Application.Interfaces
+{
+    public interface IProviderFeedbackService
+    {
+        Task<int> SendProviderFeedbackEmailsAsync(string userName);
+    }
+}
---
+++ src/Sfa.Tl.Matching.Application/Services/ProviderFeedbackService.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Sfa.Tl.Matching.Api.Clients.Calendar;
+using Sfa.Tl.Matching.Application.Interfaces;
+using Sfa.Tl.Matching.Data.Interfaces;
+using Sfa.Tl.Matching.Models.Configuration;
+
+namespace Sfa.Tl.Matching.Application.Services
+{
+    public class ProviderFeedbackService : IProviderFeedbackService
+    {
+        private readonly MatchingConfiguration _configuration;
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private readonly IEmailService _emailService;
+        private readonly IProviderRepository _providerRepository;
+        private readonly ILogger<ProviderFeedbackService> _logger;
+
+        public ProviderFeedbackService(
+            MatchingConfiguration configuration,
+            ILogger<ProviderFeedbackService> logger,
+            IEmailService emailService,
+            IProviderRepository providerRepository,
+            IDateTimeProvider dateTimeProvider)
+        {
+            _configuration = configuration;
+            _logger = logger;
+            _emailService = emailService;
+            _providerRepository = providerRepository;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public async Task<int> SendProviderFeedbackEmailsAsync(string userName)
+        {
+            return 0;
+        }
+    }
+}
