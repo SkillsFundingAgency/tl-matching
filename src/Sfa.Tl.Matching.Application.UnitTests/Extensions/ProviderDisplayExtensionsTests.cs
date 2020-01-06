7e60d7122720cbb2c9f7f98808ab70f986875c23
Mike Wild <wildconsultingltd@googlemail.com>
2020-01-06 13:24:01 +0000

    TLWP-920 - use case-insensitive check for postcode comparison

---
+++ src/Sfa.Tl.Matching.Application.UnitTests/Extensions/ProviderDisplayExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Sfa.Tl.Matching.Models.Extensions;
+using Xunit;
+
+namespace Sfa.Tl.Matching.Application.UnitTests.Extensions
+{
+    [Trait("ProviderDisplayExtensions", "Data Tests")]
+    public class ProviderDisplayExtensionsTests
+    {
+        [Theory(DisplayName = "GetDisplayText Data Tests")]
+        [InlineData("venue", "CV1 2WT", "display name", true, "venue part of display name (CV1 2WT)")]
+        [InlineData("CV1 2WT", "CV1 2WT", "display name", true, "display name (CV1 2WT)")]
+        [InlineData("cv1 2wt", "CV1 2WT", "display name", true, "display name (CV1 2WT)")]
+        public void GetDisplayTextDataTests(string venue, string postcode, string displayName, bool includePartOf, string result)
+        {
+            var displayText = ProviderDisplayExtensions.GetDisplayText(venue, postcode, displayName, includePartOf);
+            displayText.Should().Be(result);
+        }
+        
+        [Theory(DisplayName = "GetProviderEmailDisplayText Data Tests")]
+        [InlineData("venue name", "CV1 2WT", "display name", "venue name")]
+        [InlineData("CV1 2WT", "CV1 2WT", "display name", "display name")]
+        [InlineData("cv1 2wt", "CV1 2WT", "display name", "display name")]
+        public void GetProviderEmailDisplayTextDataTests(string venue, string postcode, string displayName, string result)
+        {
+            var displayText = ProviderDisplayExtensions.GetProviderEmailDisplayText(venue, postcode, displayName);
+            displayText.Should().Be(result);
+        }
+    }
+}
