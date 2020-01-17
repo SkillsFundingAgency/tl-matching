553c3ce571dff19a6284c45ba1aacb4c473a37b2
Adam <adamesfa@outlook.com>
2020-01-17 10:04:52 +0000

    Update EmailTemplateId for Secondary emails

--- src/Sfa.Tl.Matching.Database/PreDeployment/PreDeploy.sql
+++ src/Sfa.Tl.Matching.Database/PreDeployment/PreDeploy.sql
@@ -2,3 +2,25 @@
  Pre-Deployment Script
 */
 
+/* Update EmailHistory EmailTemplateId for SecondaryProviderReferral */
+DECLARE @PrimaryEmailTemplateId int
+SET @PrimaryEmailTemplateId = (SELECT Id 
+							FROM EmailTemplate
+							WHERE TemplateName = 'ProviderReferralV4')
+
+DECLARE @SecondaryEmailTemplateId int
+SET @SecondaryEmailTemplateId = (SELECT Id 
+							FROM EmailTemplate
+							WHERE TemplateName = 'SecondaryProviderReferral')
+
+UPDATE EmailHistory
+SET EmailTemplateId = @SecondaryEmailTemplateId
+FROM Provider p
+JOIN EmailHistory eh
+	ON eh.SentTo = p.SecondaryContactEmail
+JOIN EmailTemplate et
+	ON et.Id = eh.EmailTemplateId
+WHERE eh.EmailTemplateId = @PrimaryEmailTemplateId
+	AND p.Id NOT IN (SELECT p1.Id
+					FROM Provider p1
+					WHERE p1.PrimaryContactEmail = p1.SecondaryContactEmail)
