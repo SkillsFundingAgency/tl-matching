a6e3c3127266b24a5c93bdee1d147d18d1c72d2c
Mike Wild <wildconsultingltd@googlemail.com>
2020-06-24 11:25:54 +0100

    Add error logging to manual Bank Holday update

--- src/Sfa.Tl.Matching.Functions/BankHolidayGenerator.cs
+++ src/Sfa.Tl.Matching.Functions/BankHolidayGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Http;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -67,28 +68,47 @@ namespace Sfa.Tl.Matching.Functions
             ILogger logger,
             [Inject] ICalendarApiClient calendarApiClient,
             [Inject] IMapper mapper,
-            [Inject] IBulkInsertRepository<BankHoliday> bankHolidayBulkInsertRepository)
+            [Inject] IBulkInsertRepository<BankHoliday> bankHolidayBulkInsertRepository,
+            [Inject] IRepository<FunctionLog> functionLogRepository)
         {
-            logger.LogInformation($"Function {context.FunctionName} triggered");
+            try
+            {
+                logger.LogInformation($"Function {context.FunctionName} triggered");
 
-            var stopwatch = Stopwatch.StartNew();
-            var holidays = await calendarApiClient.GetBankHolidaysAsync();
-            var createdRecords = await SaveHolidaysAsync(holidays, bankHolidayBulkInsertRepository, mapper);
-            stopwatch.Stop();
+                var stopwatch = Stopwatch.StartNew();
+                var holidays = await calendarApiClient.GetBankHolidaysAsync();
+                var createdRecords = await SaveHolidaysAsync(holidays, bankHolidayBulkInsertRepository, mapper);
+                stopwatch.Stop();
 
-            logger.LogInformation($"Function {context.FunctionName} finished processing\n" +
-                                  $"\tRows saved: {createdRecords}\n" +
-                                  $"\tTime taken: {stopwatch.ElapsedMilliseconds: #,###}ms");
+                logger.LogInformation($"Function {context.FunctionName} finished processing\n" +
+                                      $"\tRows saved: {createdRecords}\n" +
+                                      $"\tTime taken: {stopwatch.ElapsedMilliseconds: #,###}ms");
 
-            return new OkObjectResult($"{createdRecords} records created.");
+                return new OkObjectResult($"{createdRecords} records created.");
+            }
+            catch (Exception e)
+            {
+                var errormessage = $"Error loading Bank Holiday Data. Internal Error Message {e}";
+
+                logger.LogError(errormessage);
+
+                await functionLogRepository.CreateAsync(new FunctionLog
+                {
+                    ErrorMessage = errormessage,
+                    FunctionName = context.FunctionName,
+                    RowNumber = -1
+                });
+
+                return new InternalServerErrorResult();
+            }
         }
 
-        private async Task<int> SaveHolidaysAsync(IList<BankHolidayResultDto> holidays, 
+        private async Task<int> SaveHolidaysAsync(IList<BankHolidayResultDto> holidays,
             IBulkInsertRepository<BankHoliday> bankHolidayBulkInsertRepository,
             IMapper mapper)
         {
             var entities = mapper.Map<IList<BankHoliday>>(holidays).ToList();
-            
+
             await bankHolidayBulkInsertRepository.BulkInsertAsync(entities);
 
             return entities.Count;
