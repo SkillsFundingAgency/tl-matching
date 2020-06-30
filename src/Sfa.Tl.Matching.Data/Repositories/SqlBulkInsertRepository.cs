368a702d175552b080abd9f59e360acf049d524b
Mike Wild <wildconsultingltd@googlemail.com>
2020-06-30 11:48:32 +0100

    Add Microsft.Data.SqlClient to SqlBulkInsertRepository

--- src/Sfa.Tl.Matching.Data/Repositories/SqlBulkInsertRepository.cs
+++ src/Sfa.Tl.Matching.Data/Repositories/SqlBulkInsertRepository.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Transactions;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Sfa.Tl.Matching.Data.Extensions;
 using Sfa.Tl.Matching.Data.Interfaces;
