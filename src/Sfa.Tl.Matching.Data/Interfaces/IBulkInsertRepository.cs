06e211b02f085cf27872dc5f1ccca84db1276393
Mike Wild <wildconsultingltd@googlemail.com>
2020-07-16 14:26:02 +0100

    Make IBulkInsertRepository<T> contravariant
    Generics fix suggested by ReSharper

--- src/Sfa.Tl.Matching.Data/Interfaces/IBulkInsertRepository.cs
+++ src/Sfa.Tl.Matching.Data/Interfaces/IBulkInsertRepository.cs
@@ -4,7 +4,7 @@ using Sfa.Tl.Matching.Domain.Models;
 
 namespace Sfa.Tl.Matching.Data.Interfaces
 {
-    public interface IBulkInsertRepository<T> where T : BaseEntity, new()
+    public interface IBulkInsertRepository<in T> where T : BaseEntity, new()
     {
         Task BulkInsertAsync(IEnumerable<T> entities);
         Task<int> MergeFromStagingAsync(bool deleteMissingRows = true);
