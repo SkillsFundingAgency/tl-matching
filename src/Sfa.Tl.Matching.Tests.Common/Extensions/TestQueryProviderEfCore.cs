66831b7221dc4553a233e7dc13b2ae4bf8af166f
Jay Freeman <byronbayer@gmail.com>
2020-11-18 03:13:05 +0000

    Revert "Updated packages"
    This reverts commit 52fe1e039193f2fb2d4e178efd624ab786300641.

--- src/Sfa.Tl.Matching.Tests.Common/Extensions/TestQueryProviderEfCore.cs
+++ src/Sfa.Tl.Matching.Tests.Common/Extensions/TestQueryProviderEfCore.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace Sfa.Tl.Matching.Tests.Common.Extensions
