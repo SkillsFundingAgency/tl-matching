52fe1e039193f2fb2d4e178efd624ab786300641
Jay Freeman <byronbayer@gmail.com>
2020-11-18 02:58:01 +0000

    Updated packages

--- src/Sfa.Tl.Matching.Tests.Common/Extensions/TestQueryProviderEfCore.cs
+++ src/Sfa.Tl.Matching.Tests.Common/Extensions/TestQueryProviderEfCore.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace Sfa.Tl.Matching.Tests.Common.Extensions
