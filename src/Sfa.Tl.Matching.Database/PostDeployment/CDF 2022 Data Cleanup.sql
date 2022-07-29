c6cf09d64c2d645bfa37e512ff90e82796e3cd09
Mike Wild <wildconsultingltd@googlemail.com>
2022-07-29 14:09:21 +0100

    Add empty data cleanup scripts

---
+++ src/Sfa.Tl.Matching.Database/PostDeployment/CDF 2022 Data Cleanup.sql
@@ -0,0 +1,17 @@
+﻿--TLWP-1720 - Clear out existing data
+
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+SET @scriptName = 'CDF 2022 Data Cleanup';
+SET @TicketNo  = 'TLWP-1720';
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[DBProjDeployLog] WHERE [Name] = @scriptName)
+BEGIN
+
+	--TODO: Add script
+	SELECT * FROM [dbo].[Providers];
+
+	--Update deployment log
+    --INSERT INTO [dbo].[DBProjDeployLog]( [Date], [Name], [MD5], [Revision] )
+	--VALUES( GETUTCDATE(), @scriptName, @TicketNo, 1 );
+END
---
+++ src/Sfa.Tl.Matching.Database/PostDeployment/CDF 2022 Qualifications Update.sql
@@ -0,0 +1,17 @@
+﻿--TLWP-1721 - Update qualifications
+
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+SET @scriptName = 'CDF 2022 Qualifications Update';
+SET @TicketNo  = 'TLWP-1721';
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[DBProjDeployLog] WHERE [Name] = @scriptName)
+BEGIN
+
+	--TODO: Add script
+	SELECT * FROM [dbo].[Providers];
+
+	--Update deployment log
+    --INSERT INTO [dbo].[DBProjDeployLog]( [Date], [Name], [MD5], [Revision] )
+	--VALUES( GETUTCDATE(), @scriptName, @TicketNo, 1 );
+END
