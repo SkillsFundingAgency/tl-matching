6652857bfb17d294712ff6bc311139e57f434fe2
Mike Wild <wildconsultingltd@googlemail.com>
2022-09-27 16:40:28 +0100

    Increase bank holiday name length
     - broken by 'Bank Holiday for the State Funeral of Queen Elizabeth II'

--- src/Sfa.Tl.Matching.Database/Tables/BankHoliday.sql
+++ src/Sfa.Tl.Matching.Database/Tables/BankHoliday.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL IDENTITY(1,1), 
 	[Date] DATE NOT NULL, 
-	[Title] NVARCHAR(50) NOT NULL,
+	[Title] NVARCHAR(100) NOT NULL,
 	[CreatedOn] DATETIME2 NOT NULL DEFAULT getutcdate(), 
 	[CreatedBy] NVARCHAR(50) NULL, 
 	[ModifiedOn] DATETIME2 NULL, 
